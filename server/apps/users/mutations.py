4ea16f3ef6db0328a0c574936a46927b8b94d51a
annshress <ann.shress@gmail.com>
2020-08-07 13:00:52 +0545

    add register mutation

--- server/apps/users/mutations.py
+++ server/apps/users/mutations.py
@@ -2,10 +2,15 @@ import graphene
 from django.contrib.auth import get_user_model, login
 from graphene_django.forms.mutation import DjangoFormMutation
 
-from apps.users.forms import LoginForm
+from apps.users.forms import LoginForm, RegisterForm
 from apps.users.schema import UserType
 
 User = get_user_model()
+
+
+class RegisterMutation(DjangoFormMutation):
+    class Meta:
+        form_class = RegisterForm
 
 
 class LoginMutation(DjangoFormMutation):
@@ -17,10 +22,10 @@ class LoginMutation(DjangoFormMutation):
     @classmethod
     def perform_mutate(cls, form, info):
         if user := form.cleaned_data.get('user', None):
-            print('form has the user', user)
             login(info.context, user)
         return super().perform_mutate(form, info)
 
 
 class Mutation(object):
     login = LoginMutation.Field()
+    register = RegisterMutation.Field()
