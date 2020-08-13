ea2afa1916a075a4c4e2e01798e9482ed8d339dc
annshress <ann.shress@gmail.com>
2020-08-13 09:27:24 +0545

    Update database configuration

--- server/helix/settings.py
+++ server/helix/settings.py
@@ -91,14 +91,8 @@ WSGI_APPLICATION = 'helix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 if os.environ.get('GITHUB_WORKFLOW'):
+    print('Database github workflow')
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -108,6 +102,13 @@ if os.environ.get('GITHUB_WORKFLOW'):
             'PASSWORD': 'postgres',
             'HOST': 'localhost',
             'PORT': 5432,
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
