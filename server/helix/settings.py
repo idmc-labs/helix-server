99774761fa361151008d39758bef4fffb9a85946
annshress <ann.shress@gmail.com>
2020-08-13 09:20:45 +0545

    Use postgres for github workflows

--- server/helix/settings.py
+++ server/helix/settings.py
@@ -98,6 +98,18 @@ DATABASES = {
     }
 }
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            # in the workflow environment
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+            'PORT': 5432,
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
