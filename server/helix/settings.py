ec38f406db15bb4508881d100cae0e0dc86f8d98
annshress <ann.shress@gmail.com>
2020-08-26 10:13:08 +0545

    Remove redundant postgres Dockerfile

--- server/helix/settings.py
+++ server/helix/settings.py
@@ -38,7 +38,7 @@ LOCAL_APPS = [
     'contact',
     'crisis',
     'event',
-    'entry',
+    # 'entry',
 ]
 
 THIRD_PARTY_APPS = [
@@ -117,7 +117,7 @@ else:
             'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
             'USER': os.environ.get('POSTGRES_USER', 'postgres'),
             'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-            'HOST': 'db',
+            'HOST': os.environ.get('POSTGRES_HOST', 'db'),
             'PORT': os.environ.get('POSTGRES_PORT', 5432),
         }
     }
