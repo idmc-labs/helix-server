58508a741014e3cc435692f9d63597db13c9e285
annshress <ann.shress@gmail.com>
2020-08-28 13:13:30 +0545

    Add figure.uuid field to populate array error keys

--- server/apps/entry/tests/test_apis.py
+++ server/apps/entry/tests/test_apis.py
@@ -402,12 +402,6 @@ class TestEntryCreation(HelixGraphQLTestCase):
                          content['data']['createEntry']['errors'][0]['arrayErrors'][0]['objectErrors'][0]['field'])
         self.assertEqual(uuid_error,
                          content['data']['createEntry']['errors'][0]['arrayErrors'][0]['key'])
-        # self.assertTrue(content['data']['createEntry']['ok'], content)
-        # self.assertIsNone(content['data']['createEntry']['errors'], content)
-        # self.assertIsNotNone(content['data']['createEntry']['entry']['id'])
-        # self.assertEqual(content['data']['createEntry']['entry']['figures']['totalCount'],
-        #                  len(figures))
-        # self.assertIsNotNone(content['data']['createEntry']['entry']['figures']['results'][0]['id'])
 
     def test_invalid_reviewer_entry_create(self):
         reviewer = create_user_with_role(role=MONITORING_EXPERT_REVIEWER)
@@ -464,6 +458,7 @@ class TestEntryCreation(HelixGraphQLTestCase):
         self.assertResponseNoErrors(response)
         self.assertFalse(content['data']['createEntry']['ok'], content)
         self.assertIn('ageTo', json.dumps(content['data']['createEntry']['errors']))
+        self.assertIn(PERMISSION_DENIED_MESSAGE, content['errors'][0]['message'])
 
     def test_create_entry_with_document(self):
         file_text = b'fake blaa'
