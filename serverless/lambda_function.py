11bf675936c835cec8ae03b664b3b7f4afed35cc
annshress <ann.shress@gmail.com>
2020-09-22 14:15:35 +0545

    Use wkhtmltopdf layer arn
    Update sls config for layers
    
    Update config to include s3 buckets

--- serverless/lambda_function.py
+++ serverless/lambda_function.py
@@ -5,33 +5,43 @@ except ImportError:
 import os
 from subprocess import call
 import sys
+import logging
+import boto3
 
-# import boto3
+logger = logging.getLogger(__name__)
+client = boto3.client('s3')
+S3_BUCKET_NAME = 'togglecorp-helix'
 
 
 def handle(event, context):
     url = event.get('url', 'https://google.com')
+    filename = event.get('filename', 'test.pdf')
 
     import pdfkit
-    config = pdfkit.configuration(wkhtmltopdf='binary/wkhtmltopdf')
+    config = pdfkit.configuration(wkhtmltopdf='/opt/bin/wkhtmltopdf')
     try:
         pdf_content = pdfkit.from_url(url, False, configuration=config)
     except Exception as e:
-        print(e)
+        logger.exception(e)
         response = {
             "statusCode": 500,
             "body": 'Unable to generate pdf.'
         }
         return response
 
-    # s3 = boto3.client('s3')
-    # s3.upload_fileobj(pdf_content,
-    #                   os.environ.get('S3_BUCKET_NAME'),
-    #                   event['file_name'])
+    r = client.put_object(
+        ACL='public-read',
+        Body=pdf_content,
+        ContentType='application/pdf',
+        Bucket=S3_BUCKET_NAME,
+        Key=filename
+    )
+
+    object_url = "https://{0}.s3.amazonaws.com/{1}".format(S3_BUCKET_NAME, filename)
 
     response = {
         "statusCode": 200,
-        "body": pdf_content
+        "body": object_url
     }
 
     return response
