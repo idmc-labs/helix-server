5b02960c5e5d864579bbc87f3b3a6639a18cfd0b
Rishi <khatririshi2430@gmail.com>
2021-02-10 14:06:37 +0545

    Add validations in figures

--- apps/entry/tests/test_apis.py
+++ apps/entry/tests/test_apis.py
@@ -190,6 +190,7 @@ class TestCreateFigure(HelixGraphQLTestCase):
             self.mutation,
             input_data=self.input
         )
+        
         content = json.loads(response.content)
         self.assertResponseNoErrors(response)
         self.assertFalse(content['data']['createFigure']['ok'], content)
