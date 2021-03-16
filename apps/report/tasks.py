a8b64d3ad296ce7c7804ef6d6e6084e59df71eab
annshress <ann.shress@gmail.com>
2021-03-16 09:55:47 +0545

    Remove illegal characters from snapshot

--- apps/report/tasks.py
+++ apps/report/tasks.py
@@ -1,10 +1,14 @@
 import itertools
 import logging
+import re
+import time
 from tempfile import NamedTemporaryFile
 
-import dramatiq
 from django.core.files.base import ContentFile
+from django.db import transaction
+import dramatiq
 from openpyxl import Workbook
+from openpyxl.cell.cell import ILLEGAL_CHARACTERS_RE
 
 from helix.settings import QueuePriority
 
@@ -17,69 +21,62 @@ logger = logging.getLogger(__name__)
 def generate_report_excel(generation_id):
     from apps.report.models import ReportGeneration
     generation = ReportGeneration.objects.get(id=generation_id)
-    generation.status = ReportGeneration.REPORT_GENERATION_STATUS.IN_PROGRESS
-    generation.save(update_fields=['status'])
     wb = Workbook()
 
-    try:
-        for sheet_name, sheet_data in generation.get_excel_sheets_data().items():
-            headers = sheet_data['headers']
-            data = sheet_data['data']
-            formulae = sheet_data['formulae']
-            aggregation = sheet_data.get('aggregation', None)
+    for sheet_name, sheet_data in generation.get_excel_sheets_data().items():
+        headers = sheet_data['headers']
+        data = sheet_data['data']
+        formulae = sheet_data['formulae']
+        aggregation = sheet_data.get('aggregation', None)
 
-            ws = wb.create_sheet(sheet_name)
-            # primary headers and data
-            for idx, (header_key, header_val) in enumerate(headers.items()):
-                ws.cell(column=idx + 1, row=1, value=header_val)
-                for idy, datum in enumerate(data):
-                    ws.cell(column=idx + 1, row=idy + 2, value=datum[header_key])
-            # secondary headers and data
-            idx2 = 0
-            for idx2, (header_key, formula) in enumerate(formulae.items()):
-                # column starts at 1, hence idx+idx2+2
-                ws.cell(column=idx + idx2 + 2, row=1, value=header_key)
-                # list indexing starts at 0, hence idx+idx2+1
-                for row, cell in enumerate(list(ws.columns)[idx + idx2 + 1], 1):
-                    if row == 1:
-                        continue
-                    cell.value = formula.format(row=row)
-            # add a gap
-            column_at = idx + idx2 + 2
-            ws.cell(column=column_at, row=1, value='')
+        ws = wb.create_sheet(sheet_name)
+        # primary headers and data
+        for idx, (header_key, header_val) in enumerate(headers.items()):
+            ws.cell(column=idx + 1, row=1, value=header_val)
+            for idy, datum in enumerate(data):
+                ws.cell(column=idx + 1, row=idy + 2, value=datum[header_key])
+        # secondary headers and data
+        idx2 = 0
+        for idx2, (header_key, formula) in enumerate(formulae.items()):
+            # column starts at 1, hence idx+idx2+2
+            ws.cell(column=idx + idx2 + 2, row=1, value=header_key)
+            # list indexing starts at 0, hence idx+idx2+1
+            for row, cell in enumerate(list(ws.columns)[idx + idx2 + 1], 1):
+                if row == 1:
+                    continue
+                cell.value = formula.format(row=row)
+        # add a gap
+        column_at = idx + idx2 + 2
+        ws.cell(column=column_at, row=1, value='')
 
-            if not aggregation:
-                continue
-            agg_headers = aggregation['headers']
-            agg_data = aggregation['data']
-            agg_formulae = aggregation['formulae']
+        if not aggregation:
+            continue
+        agg_headers = aggregation['headers']
+        agg_data = aggregation['data']
+        agg_formulae = aggregation['formulae']
 
-            # primary headers and data
-            for idx, (header_key, header_val) in enumerate(agg_headers.items()):
-                ws.cell(column=column_at + idx + 1, row=1, value=header_val)
-                for idy, datum in enumerate(agg_data):
-                    ws.cell(column=column_at + idx + 1, row=idy + 2, value=datum[header_key])
-            # secondary headers and data
-            for idx2, (header_key, formula) in enumerate(agg_formulae.items()):
-                # column starts at 1, hence idx+idx2+2
-                ws.cell(column=column_at + idx + idx2 + 2, row=1, value=header_key)
-                # list indexing starts at 0, hence idx+idx2+1
-                for row, cell in enumerate(list(ws.columns)[column_at + idx + idx2 + 1], 1):
-                    if row == 1:
-                        continue
-                    cell.value = formula.format(row=row)
+        # primary headers and data
+        for idx, (header_key, header_val) in enumerate(agg_headers.items()):
+            ws.cell(column=column_at + idx + 1, row=1, value=header_val)
+            for idy, datum in enumerate(agg_data):
+                ws.cell(column=column_at + idx + 1, row=idy + 2, value=datum[header_key])
+        # secondary headers and data
+        for idx2, (header_key, formula) in enumerate(agg_formulae.items()):
+            # column starts at 1, hence idx+idx2+2
+            ws.cell(column=column_at + idx + idx2 + 2, row=1, value=header_key)
+            # list indexing starts at 0, hence idx+idx2+1
+            for row, cell in enumerate(list(ws.columns)[column_at + idx + idx2 + 1], 1):
+                if row == 1:
+                    continue
+                cell.value = formula.format(row=row)
 
-        with NamedTemporaryFile() as tmp:
-            wb.save(tmp.name)
-            tmp.seek(0)
-            content = tmp.read()
-            path = f'{generation.report.name}.xlsx'
-            generation.full_report.save(path, ContentFile(content))
-        generation.status = ReportGeneration.REPORT_GENERATION_STATUS.COMPLETED
-    except Exception as e:  # NOQA E722
-        logger.error('Report Generation Failed', exc_info=True)
-        generation.status = ReportGeneration.REPORT_GENERATION_STATUS.FAILED
-
+    with NamedTemporaryFile() as tmp:
+        wb.save(tmp.name)
+        tmp.seek(0)
+        content = tmp.read()
+        path = f'{generation.report.name}.xlsx'
+        generation.full_report.save(path, ContentFile(content))
+    generation.status = ReportGeneration.REPORT_GENERATION_STATUS.COMPLETED
     generation.save(update_fields=['status'])
 
 
@@ -94,7 +91,7 @@ def generate_report_snapshot(generation_id):
         headers = list(set(itertools.chain.from_iterable(sheet_data)))
         ws.append(headers)
         for elements in sheet_data:
-            ws.append([str(elements.get(header)) for header in headers])
+            ws.append([re.sub(ILLEGAL_CHARACTERS_RE, '', str(elements.get(header))) for header in headers])
     with NamedTemporaryFile() as tmp:
         wb.save(tmp.name)
         tmp.seek(0)
@@ -105,10 +102,20 @@ def generate_report_snapshot(generation_id):
 
 @dramatiq.actor(queue_name=QueuePriority.HEAVY.value, max_retries=3, time_limit=REPORT_TIMEOUT)
 def trigger_report_generation(generation_id):
-    from django.db import transaction
-    with transaction.atomic():
-        logger.warn('Starting report generation...')
-        generate_report_excel(generation_id)
-        logger.warn('Completed report generation')
-        generate_report_snapshot(generation_id)
-        logger.warn('Completed snapshot generation')
+    from apps.report.models import ReportGeneration
+    generation = ReportGeneration.objects.get(id=generation_id)
+    generation.status = ReportGeneration.REPORT_GENERATION_STATUS.IN_PROGRESS
+    generation.save(update_fields=['status'])
+    try:
+        with transaction.atomic():
+            then = time.time()
+            logger.warn('Starting report generation...')
+            generate_report_excel(generation_id)
+            logger.warn(f'Completed report generation in {time.time() - then}')
+            then = time.time()
+            generate_report_snapshot(generation_id)
+            logger.warn(f'Completed snapshot generation {time.time() - then}')
+    except Exception as e:  # NOQA E722
+        logger.error('Report Generation Failed', exc_info=True)
+        generation.status = ReportGeneration.REPORT_GENERATION_STATUS.FAILED
+        generation.save(update_fields=['status'])
