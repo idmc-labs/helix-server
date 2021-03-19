e804d7b59d4a67edff8bbeff4f8edf22b63a4fc9
annshress <ann.shress@gmail.com>
2021-03-19 16:32:35 +0545

    Add Date accuracy fields in crisis, event and entry

--- apps/contrib/enums.py
+++ apps/contrib/enums.py
@@ -1,7 +1,6 @@
 import graphene
 
 from apps.contrib.models import Attachment, SourcePreview
-
 from apps.contact.enums import enum_map as contact_enums
 from apps.crisis.enums import enum_map as crisis_enums
 from apps.entry.enums import enum_map as entry_enums
@@ -12,13 +11,18 @@ from apps.users.enums import enum_map as user_enums
 from apps.report.enums import enum_map as report_enums
 from utils.enums import enum_description
 
+from .commons import DateAccuracyGrapheneEnum
+
+
 AttachmentForGrapheneEnum = graphene.Enum.from_enum(Attachment.FOR_CHOICES,
                                                     description=enum_description)
 PreviewStatusGrapheneEnum = graphene.Enum.from_enum(SourcePreview.PREVIEW_STATUS,
                                                     description=enum_description)
 
 enum_map = dict(
-    FOR_CHOICES=AttachmentForGrapheneEnum
+    FOR_CHOICES=AttachmentForGrapheneEnum,
+    PREVIEW_STATUS=PreviewStatusGrapheneEnum,
+    DATE_ACCURACY=DateAccuracyGrapheneEnum,
 )
 
 ENUM_TO_GRAPHENE_ENUM_MAP = {
