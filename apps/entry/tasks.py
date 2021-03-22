bdc108ff09e4d9349ef4ad68c80570336f5db109
annshress <ann.shress@gmail.com>
2021-03-22 15:02:31 +0545

    Reduce timeout for chromium webdriver below the dramatiq timeout

--- apps/entry/tasks.py
+++ apps/entry/tasks.py
@@ -1,4 +1,5 @@
 import base64
+import math
 import logging
 
 import dramatiq
@@ -10,14 +11,15 @@ from selenium.common.exceptions import TimeoutException
 from helix.settings import QueuePriority
 
 logger = logging.getLogger(__name__)
-TIMEOUT = 60  # seconds
+DRAMATIQ_TIMEOUT = 60  # seconds
+SELENIUM_TIMEOUT = math.floor(DRAMATIQ_TIMEOUT * 0.8)
 
 
-def __get_pdf_from_html(path, timeout=TIMEOUT, print_options={}):
+def __get_pdf_from_html(path, timeout=SELENIUM_TIMEOUT, print_options={}):
     browser_options = webdriver.ChromeOptions()
+    browser_options.add_argument('no-sandbox')
     browser_options.add_argument('headless')
     browser_options.add_argument('disable-gpu')
-    browser_options.add_argument('no-sandbox')
     browser_options.add_argument('disable-dev-shm-usage')
 
     browser = webdriver.Chrome(options=browser_options)
@@ -25,8 +27,11 @@ def __get_pdf_from_html(path, timeout=TIMEOUT, print_options={}):
     browser.get(path)
 
     try:
-        WebDriverWait(browser, timeout).until(lambda d: d.execute_script(
-            'document.readyState == "complete"'))
+        WebDriverWait(browser, timeout).until(
+            lambda d: d.execute_script(
+                'document.readyState == "complete"'
+            )
+        )
     except TimeoutException:
         logger.error(f'Chromium timed out for {path}', exc_info=True)
 
@@ -44,13 +49,14 @@ def __get_pdf_from_html(path, timeout=TIMEOUT, print_options={}):
 
 
 # https://dramatiq.io/guide.html#dead-letters
-@dramatiq.actor(queue_name=QueuePriority.DEFAULT.value, max_retries=3, time_limit=TIMEOUT * 1000)
+@dramatiq.actor(queue_name=QueuePriority.DEFAULT.value, max_retries=3, time_limit=DRAMATIQ_TIMEOUT * 1000)
 def generate_pdf(pk):
     from apps.contrib.models import SourcePreview
 
     source_preview = SourcePreview.objects.get(pk=pk)
     url = source_preview.url
-    path = f'{SourcePreview.PREVIEW_FOLDER}/{source_preview.token}.pdf'
+    path = f'{source_preview.token}.pdf'
+    logger.warn(f'Starting pdf generation for url: {url} and preview id: {source_preview.id}.')
 
     source_preview.status = SourcePreview.PREVIEW_STATUS.IN_PROGRESS
     source_preview.save()
@@ -60,6 +66,7 @@ def generate_pdf(pk):
         source_preview.pdf.save(path, ContentFile(pdf_content))
         source_preview.status = SourcePreview.PREVIEW_STATUS.COMPLETED
         source_preview.save()
+        logger.warn(f'Completed pdf generation for url: {url} and preview id: {source_preview.id}.')
     except Exception as e:  # noqa
         logger.error('An exception occurred', exc_info=True)
         source_preview.status = SourcePreview.PREVIEW_STATUS.FAILED
