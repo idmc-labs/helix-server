f91d1e63a4295b76ad028b975163da72a5f86ab6
annshress <ann.shress@gmail.com>
2021-04-22 15:24:30 +0545

    Add download status filter to excel downloads

--- apps/contrib/filters.py
+++ apps/contrib/filters.py
@@ -1,14 +1,28 @@
 import django_filters
 
 from apps.contrib.models import ExcelDownload
+from utils.filters import StringListFilter
 
 
 class ExcelExportFilter(django_filters.FilterSet):
+    status_list = StringListFilter(method='filter_status')
+
     class Meta:
         model = ExcelDownload
         fields = {
             'started_at': ['lt', 'gt', 'gte', 'lte']
         }
+
+    def filter_status(self, qs, name, value):
+        if value:
+            if isinstance(value[0], int):
+                # internal filtering
+                return qs.filter(status__in=value).distinct()
+            # client side filtering
+            return qs.filter(crisis_type__in=[
+                ExcelDownload.EXCEL_GENERATION_STATUS.get(item).value for item in value
+            ]).distinct()
+        return qs
 
     @property
     def qs(self):
