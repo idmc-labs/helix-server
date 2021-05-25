1e2bdc2fc3128f663c06fcbadf9c2f069052c196
annshress <ann.shress@gmail.com>
2021-05-25 13:08:23 +0545

    Allow media and static urls for debug only

--- helix/urls.py
+++ helix/urls.py
@@ -53,12 +53,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     re_path('^graphql/?$', csrf_exempt(CustomGraphQLView.as_view())),
     path('api/', include(rest_urls))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns = [
+    urlpatterns = urlpatterns + [
         path('__debug__/', include(debug_toolbar.urls)),
         re_path('^graphiql/?$', csrf_exempt(CustomGraphQLView.as_view(graphiql=True))),
-    ] + urlpatterns
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+      + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
