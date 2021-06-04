6fd020ba799d8d2cc579f9c6796aa94675a78fa9
bimal prasad pandey <pandeybi125@gmail.com>
2021-06-04 13:04:40 +0545

    Add forget passwrod mutation

--- apps/users/mutations.py
+++ apps/users/mutations.py
@@ -11,12 +11,20 @@ from apps.users.serializers import (
     ActivateSerializer,
     UserSerializer,
     UserPasswordSerializer,
+    ForgotPasswordSerializer,
+    ReSetPasswordSerializer,
 )
 from utils.permissions import is_authenticated
 from utils.error_types import CustomErrorType, mutation_is_not_valid
 from utils.mutation import generate_input_type_for_serializer
 from utils.validations import MissingCaptchaException
-
+from .tasks import send_email
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils import timezone
+from datetime import datetime, timedelta
+from django.utils.dateparse import parse_datetime
+from django.db import transaction
 
 RegisterInputType = generate_input_type_for_serializer(
     'RegisterInputType',
@@ -179,6 +187,60 @@ class UpdateUser(graphene.Mutation):
         return UpdateUser(result=user, errors=None, ok=True)
 
 
+ForgetPasswordType = generate_input_type_for_serializer(
+    'ForgetPasswordType',
+    ForgotPasswordSerializer
+)
+
+
+class ForgetPassword(graphene.Mutation):
+    class Arguments:
+        data = ForgetPasswordType(required=True)
+
+    errors = graphene.List(graphene.NonNull(CustomErrorType))
+    ok = graphene.Boolean()
+
+    @staticmethod
+    def mutate(root, info, data):
+        serializer = ForgotPasswordSerializer(data=data)
+        if errors := mutation_is_not_valid(serializer):
+            return ForgetPassword(errors=errors, ok=False)
+        email = serializer.validated_data.get("email")
+        # if user exists for this email
+        try:
+            user = User.objects.get(email=email)
+            # generate a password reset token with user's id and token created date
+            code = urlsafe_base64_encode(force_bytes(f"{user.pk},{timezone.now() + timedelta(hours=24)}"))
+            base_url = settings.FRONTEND_BASE_URL
+            # Get base url by profile type
+            button_url = f"{base_url}/reset-password/?password_reset_token={code}"
+            message = (
+                "We received a request to reset your Helix account password. "
+                "If you wish to do so, please click below. Otherwise, you may "
+                "safely disregard this email."
+            )
+        # if no user exists for this email
+        except User.DoesNotExist:
+            # explanatory email message
+            return ForgetPassword(
+                errors=[
+                    dict(field='nonFieldErrors', messages=gettext(f'User with this email {email} does not exists.'))
+                ]
+            )
+        subject = "Reset password request for Helix"
+        context = {
+            "heading": "Reset Password",
+            "message": message,
+            "button_text": "Reset Password",
+        }
+        if button_url:
+            context["button_url"] = button_url
+        transaction.on_commit(lambda: send_email(
+                subject, message, [email], html_context=context
+        ))
+        return ForgetPassword(errors=None, ok=True)
+        
+
 class Mutation(object):
     login = Login.Field()
     register = Register.Field()
@@ -186,3 +248,5 @@ class Mutation(object):
     logout = Logout.Field()
     update_user = UpdateUser.Field()
     change_password = ChangeUserPassword.Field()
+    forget_password = ForgetPassword.Field()
+    rest_password = ResetPassword.Field()
