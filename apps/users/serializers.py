daf7f3b798cb0531a6d31f4d9ff9ec58e02d83e9
bimal prasad pandey <pandeybi125@gmail.com>
2021-06-17 11:53:48 +0545

    Improve error message

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -219,7 +219,7 @@ class GenerateResetPasswordTokenSerializer(serializers.Serializer):
         # if no user exists for this email
         except User.DoesNotExist:
             # explanatory email message
-            raise serializers.ValidationError(gettext('User with this email does not exists.'))
+            raise serializers.ValidationError(gettext('User with this email does not exist.'))
         subject = gettext("Reset password request for Helix")
         context = {
             "heading": gettext("Reset Password"),
