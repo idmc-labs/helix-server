184b22a158c40cd449220cab650f650d91c954b4
tnagorra <weathermist@gmail.com>
2021-07-01 19:43:53 +0545

    Update user creation

--- apps/users/management/commands/create_dummy_users.py
+++ apps/users/management/commands/create_dummy_users.py
@@ -8,29 +8,28 @@ from apps.users.models import Portfolio
 class Command(BaseCommand):
     help = 'Create dummy users.'
 
+    def add_arguments(self, parser):
+        parser.add_argument('firstname', type=str)
+        parser.add_argument('lastname', type=str)
+        parser.add_argument('email', type=str)
+        parser.add_argument('password', type=str)
+        parser.add_argument('role', type=str)
+
     def handle(self, *args, **options):
         User = get_user_model()
-        raw_password = 'admin123'
-        roles = [('admin', USER_ROLE.ADMIN, 'Eric', 'Lowe'),
-                 ('guest', USER_ROLE.GUEST, 'Frederick', 'Gutierrez')]
 
-        for username, role, first_name, last_name in roles:
-            email = f'{username}@idmcdb.org'
-            user, _ = User.objects.get_or_create(
-                email=email,
-            )
-            user.username = username
-            user.first_name = first_name
-            user.last_name = last_name
-            user.set_password(raw_password)
-            user.save()
-            self.stdout.write(self.style.SUCCESS(f'{role.name} created with email: '
-                                                 f'{email} password: {raw_password}'))
-            try:
-                Portfolio.objects.get_or_create(
-                    user=user,
-                    role=role,
-                )
-                self.stdout.write(self.style.SUCCESS(f'Added portfolio for {role.name}\n'))
-            except Exception as e:
-                print('Failed here...', email, e, ' << Ignore me\n')
+        user, _ = User.objects.get_or_create(
+            email=options['email'],
+        )
+        user.username = options['email']
+        user.first_name = options['firstname']
+        user.last_name = options['lastname']
+        user.set_password(options['jassword'])
+        user.save()
+
+        role = USER_ROLE.ADMIN if options['role'] == 'admin' else USER_ROLE.GUEST
+
+        Portfolio.objects.get_or_create(
+            user=user,
+            role=role,
+        )
