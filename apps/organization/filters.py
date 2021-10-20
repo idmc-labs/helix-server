9287b54ba275d78bfa02a4ddce28f82b52326d95
Bimal prasad pandey <pandeybi125@gmail.com>
2021-10-20 15:36:28 +0545

    Add new filters in organization

--- apps/organization/filters.py
+++ apps/organization/filters.py
@@ -1,13 +1,36 @@
 import django_filters
-
 from apps.organization.models import Organization
-from utils.filters import NameFilterMixin
+from utils.filters import NameFilterMixin, IDListFilter
 
 
 class OrganizationFilter(NameFilterMixin,
                          django_filters.FilterSet):
     name = django_filters.CharFilter(method='_filter_name')
+    countries = IDListFilter(method='filter_countries')
+    categories = IDListFilter(method='filter_categories')
+    organization_types = IDListFilter(method='filter_organization_types')
 
     class Meta:
         model = Organization
-        fields = []
+        fields = {
+            'short_name': ['unaccent__icontains'],
+        }
+
+    def filter_countries(self, qs, name, value):
+        if not value:
+            return qs
+        return qs.filter(countries__in=value).distinct()
+
+    def filter_categories(self, qs, name, value):
+        if not value:
+            return qs
+        return qs.filter(category__in=value).distinct()
+
+    def filter_organization_types(self, qs, name, value):
+        if not value:
+            return qs
+        return qs.filter(organization_kind__in=value).distinct()
+
+    @property
+    def qs(self):
+        return super().qs.select_related('countries', 'category', 'organization_kind')
