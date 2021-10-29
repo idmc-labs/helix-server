77e0305f6417027addfce679732b26330cf4a674
Bimal prasad pandey <pandeybi125@gmail.com>
2021-10-29 08:25:51 +0545

    Add qa recommened fiture type in query

--- apps/event/enums.py
+++ apps/event/enums.py
@@ -5,10 +5,13 @@ import graphene
 from apps.event.models import Event
 
 from utils.enums import enum_description
+from apps.common.enums import QA_RECOMMENDED_FIGURE_TYPE
 
 EventOtherSubTypeEnum = graphene.Enum.from_enum(Event.EVENT_OTHER_SUB_TYPE,
                                                 description=enum_description)
+QaRecommendedFigureEnum = graphene.Enum.from_enum(QA_RECOMMENDED_FIGURE_TYPE, description=enum_description)
 
 enum_map = dict(
-    EVENT_OTHER_SUB_TYPE=EventOtherSubTypeEnum
+    EVENT_OTHER_SUB_TYPE=EventOtherSubTypeEnum,
+    QA_RECOMMENDED_FIGURE_TYPE=QaRecommendedFigureEnum,
 )
