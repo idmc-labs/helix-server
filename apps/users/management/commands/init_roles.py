588902429113f434fb255dfed113d186d363fbf4
tnagorra <weathermist@gmail.com>
2022-03-22 11:47:03 +0545

    Fix problem with Permission object initialization

--- apps/users/management/commands/init_roles.py
+++ apps/users/management/commands/init_roles.py
@@ -8,12 +8,13 @@ class Command(BaseCommand):
     help = 'Initialize or update roles.'
 
     def handle(self, *args, **options):
+        permission_queryset = Permission.objects.exclude(content_type__app_label='helixmigration')
         for role in USER_ROLES:
             group, created = Group.objects.get_or_create(name=role.name)
             permissions = list()
             for action, models in PERMISSIONS[role].items():
                 permissions.extend([
-                    Permission.objects.get(codename=f'{action.name}_{model.name}') for model in models
+                    permission_queryset.get(codename=f'{action.name}_{model.name}') for model in models
                 ])
             group.permissions.set(permissions)
             self.stdout.write(self.style.SUCCESS(f'{"Created" if created else "Updated"} '
