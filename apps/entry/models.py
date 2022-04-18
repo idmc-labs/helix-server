ab970645aad65b48bb74ce8186a91b66bc91c0c9
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-04-18 15:10:55 +0545

    Add figure_cause, violence, violence_sub_type, disaster_category, disaster_sub_category, disaster_type, disaster_sub_type fields in figure

--- apps/entry/models.py
+++ apps/entry/models.py
@@ -259,6 +259,8 @@ class Figure(MetaInformationArchiveAbstractModel,
              UUIDAbstractModel,
              FigureDisaggregationAbstractModel,
              models.Model):
+    from apps.crisis.models import Crisis
+
     class QUANTIFIER(enum.Enum):
         MORE_THAN = 0
         LESS_THAN = 1
@@ -472,6 +474,42 @@ class Figure(MetaInformationArchiveAbstractModel,
     )
     context_of_violence = models.ManyToManyField(
         'event.ContextOfViolence', verbose_name=_('Context of violence'), blank=True, related_name='figures'
+    )
+    figure_cause = enum.EnumField(Crisis.CRISIS_TYPE, verbose_name=_('Figure Cause'))
+    violence = models.ForeignKey(
+        'event.Violence', verbose_name=_('Figure Violence'),
+        blank=False, null=True,
+        related_name='figures', on_delete=models.SET_NULL
+    )
+    violence_sub_type = models.ForeignKey(
+        'event.ViolenceSubType', verbose_name=_('Figure Violence Sub Type'),
+        blank=True, null=True,
+        related_name='figures', on_delete=models.SET_NULL
+    )
+    disaster_category = models.ForeignKey(
+        'event.DisasterCategory', verbose_name=_('Figure Disaster Category'),
+        blank=True, null=True,
+        related_name='figures', on_delete=models.SET_NULL
+    )
+    disaster_sub_category = models.ForeignKey(
+        'event.DisasterSubCategory', verbose_name=_('Figure Disaster Sub Category'),
+        blank=True, null=True,
+        related_name='figures', on_delete=models.SET_NULL
+    )
+    disaster_type = models.ForeignKey(
+        'event.DisasterType', verbose_name=_('Figure Disaster Type'),
+        blank=True, null=True,
+        related_name='figures', on_delete=models.SET_NULL
+    )
+    disaster_sub_type = models.ForeignKey(
+        'event.DisasterSubType', verbose_name=_('Figure Disaster Sub Type'),
+        blank=True, null=True,
+        related_name='figures', on_delete=models.SET_NULL
+    )
+    osv_sub_type = models.ForeignKey(
+        'event.OsvSubType', verbose_name=_('Figure OSV sub type'),
+        blank=True, null=True, related_name='figures',
+        on_delete=models.SET_NULL
     )
 
     class Meta:
