a3a8911bb3045577b3ceee89543ab213ef18e8a1
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-06-01 09:40:14 +0545

    Add commend to init context of violences

--- copilot.init.sh
+++ copilot.init.sh
@@ -15,3 +15,4 @@ python manage.py init_types_subtypes # event related
 python manage.py init_figure_tags
 python manage.py init_osv_sub_type
 python manage.py init_other_sub_types
+python manage.py init_context_of_violences
