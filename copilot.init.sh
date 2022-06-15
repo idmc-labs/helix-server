f0f7620dea951726ac3bee79e6fd8722c0735d3f
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-06-15 20:59:53 +0545

    Sync portfolio roles to django roles

--- copilot.init.sh
+++ copilot.init.sh
@@ -16,3 +16,4 @@ python manage.py init_figure_tags
 python manage.py init_osv_sub_type
 python manage.py init_other_sub_types
 python manage.py init_context_of_violences
+python manage.py save_users
