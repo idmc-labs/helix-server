119b0084bccca1610a21630f19b0c4e69a2f97b7
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-06-20 11:58:37 +0545

    Fix organization test

--- apps/organization/tests/test_serializers.py
+++ apps/organization/tests/test_serializers.py
@@ -1,7 +1,8 @@
 from django.test import RequestFactory
 
 from apps.organization.serializers import OrganizationSerializer
-from utils.tests import HelixTestCase
+from utils.tests import HelixTestCase, create_user_with_role
+from apps.users.enums import USER_ROLE
 
 
 class TestCreateOrganizationSerializer(HelixTestCase):
@@ -11,12 +12,12 @@ class TestCreateOrganizationSerializer(HelixTestCase):
             "short_name": "org1",
             "methodology": "source1",
         }
-        self.context = dict(
-            request=RequestFactory().post('/graphql')
-        )
+        self.factory = RequestFactory()
+        self.request = self.factory.get('/graphql')
+        self.request.user = self.user = create_user_with_role(USER_ROLE.MONITORING_EXPERT.name)
 
     def test_valid_serializer(self):
-        serializer = OrganizationSerializer(data=self.data, context=self.context)
+        serializer = OrganizationSerializer(data=self.data, context={'request': self.request})
         self.assertTrue(serializer.is_valid(), serializer.errors)
 
         serializer.save()
