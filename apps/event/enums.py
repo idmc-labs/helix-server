cb20464a99a4f11e9e4f5fa19519907344bc4236
bimal125 <pandeybi125@gmail.com>
2022-11-03 09:38:09 +0545

    Add review status fields

--- apps/event/enums.py
+++ apps/event/enums.py
@@ -1,9 +1,12 @@
 import graphene
 from utils.enums import enum_description
+from apps.event.models import Event
 from apps.common.enums import QA_RULE_TYPE
 
 QaRecommendedFigureEnum = graphene.Enum.from_enum(QA_RULE_TYPE, description=enum_description)
+EventReviewStatusEnum = graphene.Enum.from_enum(Event.EventReviewStatus, description=enum_description)
 
 enum_map = dict(
     QA_RULE_TYPE=QaRecommendedFigureEnum,
+    EventReviewStatus=EventReviewStatusEnum,
 )
