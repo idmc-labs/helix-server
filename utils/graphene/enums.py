d7963cb25c3b0bd5bcc342e138c5b885796a56b9
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-11-21 13:55:13 +0545

    Fix type and enums

--- utils/graphene/enums.py
+++ utils/graphene/enums.py
@@ -1,4 +1,68 @@
+from typing import Union
+
 import graphene
+from django_enumfield import enum
+from rest_framework import serializers
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
+def to_camelcase(snake_str):
+    components = snake_str.split('_')
+    return components[0] + "".join(x.title() for x in components[1:])
+
+
+def enum_description(v: enum.Enum) -> Union[str, None]:
+    try:
+        return v.label
+    except AttributeError:
+        return None
+
+
+def convert_enum_to_graphene_enum(enum, name=None, description=enum_description, deprecation_reason=None):
+    description = description or enum.__doc__
+    name = name or enum.__name__
+    meta_dict = {
+        "enum": enum,
+        "description": description,
+        "deprecation_reason": deprecation_reason,
+    }
+    meta_class = type("Meta", (object,), meta_dict)
+    return type(name, (graphene.Enum,), {"Meta": meta_class})
+
+
+def get_enum_name_from_django_field(
+    field: Union[
+        None,
+        serializers.ChoiceField,
+        models.CharField,
+        models.IntegerField,
+        ArrayField,
+        models.query_utils.DeferredAttribute,
+    ],
+    field_name=None,
+    model_name=None,
+):
+    if field_name is None and model_name is None:
+        if type(field) == serializers.ChoiceField:
+            if type(field.parent) == serializers.ListField:
+                model_name = field.parent.parent.Meta.model.__name__
+                field_name = field.parent.field_name
+            else:
+                model_name = field.parent.Meta.model.__name__
+                field_name = field.field_name
+        elif type(field) == ArrayField:
+            model_name = field.model.__name__
+            field_name = field.base_field.name
+        elif type(field) in [models.CharField, models.IntegerField]:
+            model_name = field.model.__name__
+            field_name = field.name
+        elif type(field) == models.query_utils.DeferredAttribute:
+            model_name = field.field.model.__name__
+            field_name = field.field.name
+    if model_name is None or field_name is None:
+        raise Exception(f'{field=} | {type(field)=}: Both {model_name=} and {field_name=} should have a value')
+    return f'{model_name}{to_camelcase(field_name.title())}'
 
 
 class EnumDescription(graphene.Scalar):
