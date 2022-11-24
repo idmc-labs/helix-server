59e20c5eaa9a78664390ca33bd6b0e8874f23443
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-11-24 13:39:57 +0545

    Add filters

--- apps/notification/enums.py
+++ apps/notification/enums.py
@@ -1,15 +1,11 @@
 from utils.graphene.enums import (
     convert_enum_to_graphene_enum,
-    get_enum_name_from_django_field,
 )
 
 from apps.notification.models import Notification
 
 NotificationTypeEnum = convert_enum_to_graphene_enum(Notification.Type, name='NotificationTypeEnum')
 
-enum_map = {
-    get_enum_name_from_django_field(field): enum
-    for field, enum in (
-        (Notification.type, NotificationTypeEnum),
-    )
-}
+enum_map = dict(
+    NotificationTypeEnum=NotificationTypeEnum
+)
--- apps/notification/models.py
+++ apps/notification/models.py
@@ -1,34 +1,64 @@
 from django.db import models
+from django_enumfield import enum
 from django.utils.translation import gettext_lazy as _
 
 
 class Notification(models.Model):
-    class Type(models.IntegerChoices):
-        FIGURE_APPROVED = 0, 'Figure approved'
-        FIGURE_RE_REQUESTED_REVIEW = 1, 'Figure re-requested review'
-        FIGURE_UN_APPROVED = 2, 'Figure un-approved'
-        FIGURE_CREATED = 3, 'Figure created'
-        FIGURE_UPDATED = 4, 'Figure updated'
-        FIGURE_DELETED = 5, 'Figure deleted'
-        FIGURE_UNAPPROVED_IN_SIGNED_EVENT = 6, 'Figure unapproved in signed event',
+    class Type(enum.Enum):
+        FIGURE_APPROVED = 0
+        FIGURE_RE_REQUESTED_REVIEW = 1
+        FIGURE_UN_APPROVED = 2
+        FIGURE_CREATED = 3
+        FIGURE_UPDATED = 4
+        FIGURE_DELETED = 5
+        FIGURE_UNAPPROVED_IN_SIGNED_EVENT = 6
 
-        EVENT_ASSIGNED = 101, 'Event assigned'
-        EVENT_ASSIGNEE_CLEARED = 102, 'Event assignee cleared'
-        EVENT_SIGNED_OFF = 103, 'Event signed off'
-        EVENT_SELF_ASSIGNED = 104, 'Event self assigned'
-        EVENT_APPROVED = 105, 'Event approved'
-        EVENT_INCLUDE_TRIANGULATION_CHANGED = 106, 'Event include triangulation changed'
+        EVENT_ASSIGNED = 101
+        EVENT_ASSIGNEE_CLEARED = 102
+        EVENT_SIGNED_OFF = 103
+        EVENT_SELF_ASSIGNED = 104
+        EVENT_APPROVED = 105
+        EVENT_INCLUDE_TRIANGULATION_CHANGED = 106
 
-        REVIEW_COMMENT_CREATED = 201, 'Comment created'
+        REVIEW_COMMENT_CREATED = 201
 
-    type = models.IntegerField(
-        choices=Type.choices,
+        __labels__ = {
+
+            FIGURE_APPROVED: _('Figure approved'),
+            FIGURE_RE_REQUESTED_REVIEW: _('Figure re-requested review'),
+            FIGURE_UN_APPROVED: _('Figure un-approved'),
+            FIGURE_CREATED: _('Figure created'),
+            FIGURE_UPDATED: _('Figure updated'),
+            FIGURE_DELETED: _('Figure deleted'),
+            FIGURE_UNAPPROVED_IN_SIGNED_EVENT: _('Figure unapproved in signed event'),
+
+            EVENT_ASSIGNED: _('Event assigned'),
+            EVENT_ASSIGNEE_CLEARED: _('Event assignee cleared'),
+            EVENT_SIGNED_OFF: _('Event signed off'),
+            EVENT_SELF_ASSIGNED: _('Event self assigned'),
+            EVENT_APPROVED: _('Event approved'),
+            EVENT_INCLUDE_TRIANGULATION_CHANGED: _('Event include triangulation changed'),
+            REVIEW_COMMENT_CREATED: _('Comment created'),
+        }
+
+    type = enum.EnumField(
+        enum=Type,
         verbose_name=_("Notification Type")
     )
+
     recipient = models.ForeignKey(
         'users.User',
         on_delete=models.CASCADE,
+        related_name='recipient',
         verbose_name=_('For user')
+    )
+    actor = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE,
+        related_name='actor',
+        verbose_name=_('Actor'),
+        null=True,
+        blank=True,
     )
     is_read = models.BooleanField(
         default=False,
