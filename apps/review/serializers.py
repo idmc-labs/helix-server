652f145b114137aa346fa38d1200d21267ab156f
tnagorra <weathermist@gmail.com>
2022-11-29 10:00:34 +0545

    Support empty comments when review type is "GREEN"

--- apps/review/serializers.py
+++ apps/review/serializers.py
@@ -16,14 +16,16 @@ class UnifiedReviewCommentSerializer(MetaInformationSerializerMixin, serializers
 
     def validate_comment(self, comment: str):
         # we will store null for empty bodies
-        if not comment.strip():
+        if not comment or not comment.strip():
             return None
         return comment
 
-    def validate_comment_without_reviews(self, attrs):
-        if not attrs.get('comment', '').strip():
+    def _validate_comment_without_reviews(self, attrs):
+        comment = attrs.get('comment')
+        comment_type = attrs.get('comment_type')
+        if (not comment or not comment.strip()) and comment_type != UnifiedReviewComment.REVIEW_COMMENT_TYPE.GREEN:
             raise serializers.ValidationError(dict(comment=_('Comment is empty.')))
 
     def validate(self, attrs) -> dict:
-        self.validate_comment_without_reviews(attrs)
+        self._validate_comment_without_reviews(attrs)
         return super().validate(attrs)
