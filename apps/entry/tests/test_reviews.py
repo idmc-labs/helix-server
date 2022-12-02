ee78ab8e1e0070707218df9fb003c87f26ba93d8
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-12-02 15:55:23 +0545

    Handle figure update case

--- apps/entry/tests/test_reviews.py
+++ apps/entry/tests/test_reviews.py
@@ -1,11 +1,15 @@
 import json
+from uuid import uuid4
 from apps.users.enums import USER_ROLE
-from apps.entry.models import Figure
+from apps.crisis.models import Crisis
+from apps.entry.models import Figure, OSMName
 from apps.event.models import Event
 from utils.factories import (
     EventFactory,
     FigureFactory,
+    EntryFactory,
     UnifiedReviewCommentFactory,
+    CountryFactory,
 )
 from utils.tests import HelixGraphQLTestCase, create_user_with_role
 
@@ -64,8 +68,61 @@ class TestEventReviewGraphQLTestCase(HelixGraphQLTestCase):
                 ok
                 }
             }'''
+        self.update_figure = """
+        mutation MyMutation($input: EntryUpdateInputType!) {
+          updateEntry(data: $input) {
+            ok
+            errors
+            result {
+              id
+              figures {
+                id
+                reviewStatus
+              }
+            }
+          }
+        }
+        """
         self.event = EventFactory.create(assigner=self.regional_coordinator, assignee=self.monitoring_expert)
         self.figure = FigureFactory.create(event=self.event)
+        self.country = CountryFactory.create()
+
+        source = dict(
+            uuid=str(uuid4()),
+            rank=101,
+            country=str(self.country.name),
+            countryCode=self.country.iso2,
+            osmId='ted',
+            osmType='okay',
+            displayName='okay',
+            lat=68.88,
+            lon=46.66,
+            name='name',
+            accuracy=OSMName.OSM_ACCURACY.ADM0.name,
+            identifier=OSMName.IDENTIFIER.ORIGIN.name
+        )
+        self.figures = [
+            {
+                "uuid": str(uuid4()),
+                "country": self.country.id,
+                "quantifier": Figure.QUANTIFIER.MORE_THAN.name,
+                "reported": 10,
+                "unit": Figure.UNIT.HOUSEHOLD.name,  # missing household_size
+                "term": Figure.FIGURE_TERMS.EVACUATED.name,
+                "category": Figure.FIGURE_CATEGORY_TYPES.NEW_DISPLACEMENT.name,
+                "role": Figure.ROLE.RECOMMENDED.name,
+                "startDate": "2020-10-10",
+                "includeIdu": True,
+                "excerptIdu": "excerpt abc",
+                "figureCause": Crisis.CRISIS_TYPE.CONFLICT.name,
+                "geoLocations": [source],
+                "householdSize": 20,
+                "tags": [],
+                "contextOfViolence": [],
+                "sources": [],
+            }
+
+        ]
 
     def test_all_users_can_approve_figure_except_guest(self) -> None:
         users = [self.admin, self.monitoring_expert, self.regional_coordinator]
@@ -206,3 +263,56 @@ class TestEventReviewGraphQLTestCase(HelixGraphQLTestCase):
 
         event.refresh_from_db()
         self.assertEqual(event.review_status, event.EVENT_REVIEW_STATUS.REVIEW_NOT_STARTED)
+
+    def test_should_change_figure_status_in_progress_if_figure_is_saved(self):
+        entry = EntryFactory.create()
+        event = EventFactory.create(
+            assigner=self.regional_coordinator,
+            assignee=self.regional_coordinator,
+            review_status=Event.EVENT_REVIEW_STATUS.APPROVED,
+            countries=[self.country],
+        )
+        figure = FigureFactory.create(
+            entry=entry,
+            review_status=Figure.FIGURE_REVIEW_STATUS.APPROVED,
+            event=event,
+            country=self.country,
+        )
+        figures = self.figures
+        figures[0]['event'] = event.id
+        figures[0]['country'] = self.country.id
+        figures[0]['id'] = figure.id
+
+        # No review comment case on figure
+        self.force_login(self.regional_coordinator)
+        response = self.query(
+            self.update_figure,
+            input_data={
+                'id': entry.id,
+                'figures': figures
+            }
+        )
+        content = json.loads(response.content)
+        figure_id = content['data']['updateEntry']['result']['figures'][0]['id']
+        review_status = content['data']['updateEntry']['result']['figures'][0]['reviewStatus']
+        self.assertEqual(str(figure.id), figure_id)
+        self.assertEqual(Figure.FIGURE_REVIEW_STATUS.REVIEW_NOT_STARTED.name, review_status)
+
+        # Create a review comment on figure
+        UnifiedReviewCommentFactory.create(figure=figure, event=event, comment='test comment')
+
+        figure.refresh_from_db()
+        # Review comment on figure case
+        self.force_login(self.regional_coordinator)
+        response = self.query(
+            self.update_figure,
+            input_data={
+                'id': entry.id,
+                'figures': figures
+            }
+        )
+        content = json.loads(response.content)
+        figure_id = content['data']['updateEntry']['result']['figures'][0]['id']
+        review_status = content['data']['updateEntry']['result']['figures'][0]['reviewStatus']
+        self.assertEqual(str(figure.id), figure_id)
+        self.assertEqual(Figure.FIGURE_REVIEW_STATUS.REVIEW_IN_PROGRESS.name, review_status)
