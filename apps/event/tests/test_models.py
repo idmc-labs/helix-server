2d5792fde713dd7d57cb8c1c7ec1c48f4937a0dc
Bimal Prasad Pandey <pandeybi125@gmail.com>
2022-12-07 20:58:21 +0545

    Add test for multiple event regional coordinators

--- apps/event/tests/test_models.py
+++ apps/event/tests/test_models.py
@@ -2,10 +2,17 @@ from datetime import timedelta
 from django.utils import timezone
 from apps.crisis.models import Crisis
 from apps.event.models import Event
+from apps.users.enums import USER_ROLE
 from utils.factories import (
     CrisisFactory,
     DisasterSubTypeFactory,
+    CountryFactory,
+    EventFactory,
+    MonitoringSubRegionFactory,
+    CountryRegionFactory,
+    CountrySubRegionFactory,
 )
+from utils.tests import create_user_with_role
 from utils.tests import HelixTestCase
 from utils.validations import is_child_parent_dates_valid
 
@@ -56,3 +63,69 @@ class TestGenericValidator(HelixTestCase):
         c_start = None
         errors = func(c_start, c_end, p_start, 'parent')
         self.assertFalse(errors)
+
+    def test_regional_coordinators(self):
+        # Create ME and RC in monitoring sub reigon
+        region_1 = CountryRegionFactory.create()
+        sub_region_1 = CountrySubRegionFactory.create()
+        monitoring_sub_region_1 = MonitoringSubRegionFactory.create()
+        country_1 = CountryFactory.create(
+            name='Australia',
+            monitoring_sub_region=monitoring_sub_region_1,
+            region=region_1,
+            sub_region=sub_region_1,
+        )
+        monitoring_expert_1 = create_user_with_role(
+            USER_ROLE.MONITORING_EXPERT.name,
+            monitoring_sub_region=monitoring_sub_region_1.id,
+            country=country_1.id,
+        )
+        regional_coordinator_1 = create_user_with_role(
+            USER_ROLE.REGIONAL_COORDINATOR.name,
+            country=country_1.id,
+            monitoring_sub_region=monitoring_sub_region_1.id,
+        )
+        event_1 = EventFactory.create(
+            assigner=regional_coordinator_1,
+            assignee=monitoring_expert_1,
+            countries=[country_1, ]
+        )
+
+        # Create ME and RC in another monitoring sub reigon
+        region_2 = CountryRegionFactory.create()
+        sub_region_2 = CountrySubRegionFactory.create()
+        monitoring_sub_region_2 = MonitoringSubRegionFactory.create()
+        country_2 = CountryFactory.create(
+            name='Nepal',
+            monitoring_sub_region=monitoring_sub_region_2,
+            region=region_2,
+            sub_region=sub_region_2,
+        )
+        monitoring_expert_2 = create_user_with_role(
+            USER_ROLE.MONITORING_EXPERT.name,
+            country=country_2.id,
+            monitoring_sub_region=monitoring_sub_region_2.id,
+        )
+        regional_coordinator_2 = create_user_with_role(
+            USER_ROLE.REGIONAL_COORDINATOR.name,
+            country=country_2.id,
+            monitoring_sub_region=monitoring_sub_region_2.id,
+        )
+        event_2 = EventFactory.create(
+            assigner=regional_coordinator_2,
+            assignee=monitoring_expert_2,
+            countries=[country_2, ]
+        )
+
+        # Test should return multiple reginal coordinators if actor is from different monitoring region
+        event_1_regional_coordinators = Event.regional_coordinators(event_1, actor=monitoring_expert_2)
+        event_2_regional_coordinators = Event.regional_coordinators(event_2, actor=monitoring_expert_1)
+
+        self.assertEqual(
+            set([user['id'] for user in event_1_regional_coordinators]),
+            set([regional_coordinator_1.id, regional_coordinator_2.id])
+        )
+        self.assertEqual(
+            set([user['id'] for user in event_2_regional_coordinators]),
+            set([regional_coordinator_1.id, regional_coordinator_2.id])
+        )
