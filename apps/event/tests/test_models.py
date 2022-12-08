351465c1051a20808831ad1a0c3c1e30f0e6658f
tnagorra <weathermist@gmail.com>
2022-12-08 14:56:18 +0545

    Add more tests for Event.regional_coordinators method

--- apps/event/tests/test_models.py
+++ apps/event/tests/test_models.py
@@ -65,7 +65,6 @@ class TestGenericValidator(HelixTestCase):
         self.assertFalse(errors)
 
     def test_regional_coordinators(self):
-        # Create ME and RC in monitoring sub reigon
         region_1 = CountryRegionFactory.create()
         sub_region_1 = CountrySubRegionFactory.create()
         monitoring_sub_region_1 = MonitoringSubRegionFactory.create()
@@ -75,6 +74,17 @@ class TestGenericValidator(HelixTestCase):
             region=region_1,
             sub_region=sub_region_1,
         )
+        region_2 = CountryRegionFactory.create()
+        sub_region_2 = CountrySubRegionFactory.create()
+        monitoring_sub_region_2 = MonitoringSubRegionFactory.create()
+        country_2 = CountryFactory.create(
+            name='Nepal',
+            monitoring_sub_region=monitoring_sub_region_2,
+            region=region_2,
+            sub_region=sub_region_2,
+        )
+
+        # Create ME and RC in monitoring sub region
         monitoring_expert_1 = create_user_with_role(
             USER_ROLE.MONITORING_EXPERT.name,
             monitoring_sub_region=monitoring_sub_region_1.id,
@@ -85,22 +95,8 @@ class TestGenericValidator(HelixTestCase):
             country=country_1.id,
             monitoring_sub_region=monitoring_sub_region_1.id,
         )
-        event_1 = EventFactory.create(
-            assigner=regional_coordinator_1,
-            assignee=monitoring_expert_1,
-            countries=[country_1, ]
-        )
 
-        # Create ME and RC in another monitoring sub reigon
-        region_2 = CountryRegionFactory.create()
-        sub_region_2 = CountrySubRegionFactory.create()
-        monitoring_sub_region_2 = MonitoringSubRegionFactory.create()
-        country_2 = CountryFactory.create(
-            name='Nepal',
-            monitoring_sub_region=monitoring_sub_region_2,
-            region=region_2,
-            sub_region=sub_region_2,
-        )
+        # Create ME and RC in another monitoring sub region
         monitoring_expert_2 = create_user_with_role(
             USER_ROLE.MONITORING_EXPERT.name,
             country=country_2.id,
@@ -111,21 +107,50 @@ class TestGenericValidator(HelixTestCase):
             country=country_2.id,
             monitoring_sub_region=monitoring_sub_region_2.id,
         )
+
+        event_1 = EventFactory.create(
+            assigner=regional_coordinator_1,
+            assignee=monitoring_expert_1,
+            countries=[country_1]
+        )
         event_2 = EventFactory.create(
             assigner=regional_coordinator_2,
             assignee=monitoring_expert_2,
-            countries=[country_2, ]
+            countries=[country_2]
+        )
+        event_3 = EventFactory.create(
+            assigner=regional_coordinator_1,
+            assignee=monitoring_expert_1,
+            countries=[country_1, country_2]
         )
 
-        # Test should return multiple reginal coordinators if actor is from different monitoring region
+        # Test should return single regional coordinator if actor is same monitoring region
+        event_1_only_regional_coordinators = Event.regional_coordinators(event_1, actor=monitoring_expert_1)
+        event_2_only_regional_coordinators = Event.regional_coordinators(event_2, actor=monitoring_expert_2)
+        # Test should return multiple regional coordinators if actor is from different monitoring region
         event_1_regional_coordinators = Event.regional_coordinators(event_1, actor=monitoring_expert_2)
         event_2_regional_coordinators = Event.regional_coordinators(event_2, actor=monitoring_expert_1)
 
+        # Test should return multiple regional coordinators because it has country from different monitoring regions
+        event_3_regional_coordinators = Event.regional_coordinators(event_3)
+
+        self.assertEqual(
+            set([user['id'] for user in event_1_only_regional_coordinators]),
+            set([regional_coordinator_1.id])
+        )
+        self.assertEqual(
+            set([user['id'] for user in event_2_only_regional_coordinators]),
+            set([regional_coordinator_2.id])
+        )
         self.assertEqual(
             set([user['id'] for user in event_1_regional_coordinators]),
             set([regional_coordinator_1.id, regional_coordinator_2.id])
         )
         self.assertEqual(
             set([user['id'] for user in event_2_regional_coordinators]),
+            set([regional_coordinator_1.id, regional_coordinator_2.id])
+        )
+        self.assertEqual(
+            set([user['id'] for user in event_3_regional_coordinators]),
             set([regional_coordinator_1.id, regional_coordinator_2.id])
         )
