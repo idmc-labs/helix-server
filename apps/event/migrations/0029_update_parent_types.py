d3f1a7244df60ce1fac1cf5c2a2566ac3ffc53fa
Bimal Prasad Pandey <pandeybi125@gmail.com>
2023-01-12 22:50:06 +0545

    Update parent category migraiton logic

--- apps/event/migrations/0029_update_parent_types.py
+++ apps/event/migrations/0029_update_parent_types.py
@@ -14,26 +14,39 @@ class Migration(migrations.Migration):
 
         # Update event fields
         Event.objects.filter(
-            Q(disaster_category__isnull=True, disaster_sub_category__isnull=False) |
-            Q(disaster_type__isnull=True, disaster_sub_type__isnull=False) |
+            (
+                (
+                    Q(disaster_category__isnull=True) |
+                    Q(disaster_sub_category__isnull=True) |
+                    Q(disaster_type__isnull=True)
+                ) &
+                Q(disaster_sub_type__isnull=False)
+            ) |
             Q(violence__isnull=True, violence_sub_type__isnull=False)
         ).update(
-            disaster_category=Subquery(Event.objects.filter(pk=OuterRef('pk')).values('disaster_sub_category__category')[:1]),
             disaster_type=Subquery(Event.objects.filter(pk=OuterRef('pk')).values('disaster_sub_type__type')[:1]),
+            disaster_sub_category=Subquery(Event.objects.filter(pk=OuterRef('pk')).values('disaster_sub_type__type__disaster_sub_category')[:1]),
+            disaster_category=Subquery(Event.objects.filter(pk=OuterRef('pk')).values('disaster_sub_type__type__disaster_sub_category__category')[:1]),
             violence=Subquery(Event.objects.filter(pk=OuterRef('pk')).values('violence_sub_type__violence')[:1]),
         )
 
         # Update figure fields
         Figure.objects.filter(
-            Q(disaster_category__isnull=True, disaster_sub_category__isnull=False) |
-            Q(disaster_type__isnull=True, disaster_sub_type__isnull=False) |
+            (
+                (
+                    Q(disaster_category__isnull=True) |
+                    Q(disaster_sub_category__isnull=True) |
+                    Q(disaster_type__isnull=True)
+                ) &
+                Q(disaster_sub_type__isnull=False)
+            ) |
             Q(violence__isnull=True, violence_sub_type__isnull=False)
         ).update(
-            disaster_category=Subquery(Figure.objects.filter(pk=OuterRef('pk')).values('disaster_sub_category__category')[:1]),
-            disaster_type=Subquery(Figure.objects.filter(pk=OuterRef('pk')).values('disaster_sub_type__type')[:1]),
-            violence=Subquery(Figure.objects.filter(pk=OuterRef('pk')).values('violence_sub_type__violence')[:1]),
+            disaster_type=Subquery(Event.objects.filter(pk=OuterRef('pk')).values('disaster_sub_type__type')[:1]),
+            disaster_sub_category=Subquery(Event.objects.filter(pk=OuterRef('pk')).values('disaster_sub_type__type__disaster_sub_category')[:1]),
+            disaster_category=Subquery(Event.objects.filter(pk=OuterRef('pk')).values('disaster_sub_type__type__disaster_sub_category__category')[:1]),
+            violence=Subquery(Event.objects.filter(pk=OuterRef('pk')).values('violence_sub_type__violence')[:1]),
         )
-
 
     dependencies = [
         ('event', '0028_auto_20221114_0519'),
