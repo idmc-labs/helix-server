f4b8691f183ac8dd3616c8ebfbb49f0f56cf1b54
Bimal Prasad Pandey <pandeybi125@gmail.com>
2023-04-19 18:22:55 +0545

    Add legacy conflict and disaster models

--- apps/gidd/models.py
+++ apps/gidd/models.py
@@ -32,6 +32,7 @@ class Disaster(models.Model):
         related_name='gidd_figures', on_delete=models.CASCADE,
         null=True, blank=True
     )
+    event_name = models.CharField(verbose_name=_('Event name'), max_length=256, null=True, blank=True)
     year = models.BigIntegerField()
     country = models.ForeignKey(
         'country.Country', related_name='country_disaster', on_delete=models.PROTECT,
@@ -93,3 +94,52 @@ class GiddLog(models.Model):
 
     def __str__(self):
         return str(self.triggered_at)
+
+
+class ConflictLegacy(models.Model):
+    total_displacement = models.BigIntegerField(blank=True, null=True)
+    new_displacement = models.BigIntegerField(blank=True, null=True)
+    year = models.BigIntegerField()
+    country_name = models.CharField(verbose_name=_('Name'), max_length=256, null=True, blank=True)
+    iso3 = models.CharField(verbose_name=_('ISO3'), max_length=5, null=True, blank=True)
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _('Legacy conflict')
+        verbose_name_plural = _('Legacy conflicts')
+
+    def __str__(self):
+        return str(self.id)
+
+
+class DisasterLegacy(models.Model):
+    year = models.BigIntegerField()
+    country_name = models.CharField(verbose_name=_('Name'), max_length=256, null=True, blank=True)
+    iso3 = models.CharField(verbose_name=_('ISO3'), max_length=5, null=True, blank=True)
+    event_name = models.CharField(verbose_name=_('Event name'), max_length=256, null=True, blank=True)
+
+    # Dates
+    start_date = models.DateField(blank=True, null=True)
+    start_date_accuracy = models.TextField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    end_date_accuracy = models.TextField(blank=True, null=True)
+
+    hazard_category = models.TextField(blank=True, null=True)
+    hazard_sub_category = models.TextField(blank=True, null=True)
+    hazard_sub_type = models.TextField(blank=True, null=True)
+    hazard_type = models.TextField(blank=True, null=True)
+    new_displacement = models.BigIntegerField(blank=True, null=True)
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _('Legacy disaster')
+        verbose_name_plural = _('Legacy disasters')
+
+    def __str__(self):
+        return str(self.id)
