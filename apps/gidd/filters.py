d0d812a8a290771b917a4626f4c7ebc4b38fcb6e
Bimal Prasad Pandey <pandeybi125@gmail.com>
2023-04-27 09:05:58 +0545

    Add displacement data filter

--- apps/gidd/filters.py
+++ apps/gidd/filters.py
@@ -1,11 +1,12 @@
 import django_filters
 
-from utils.filters import StringListFilter
+from utils.filters import StringListFilter, IDListFilter
 from .models import (
     Conflict,
     Disaster,
     StatusLog,
     PublicFigureAnalysis,
+    DisplacementData,
 )
 
 
@@ -100,3 +101,30 @@ class PublicFigureAnalysisFilter(django_filters.FilterSet):
             'iso3': ['exact'],
             'year': ['exact'],
         }
+
+
+class DisplacementDataFilter(django_filters.FilterSet):
+    start_year = django_filters.NumberFilter(method='filter_start_year')
+    end_year = django_filters.NumberFilter(method='filter_end_year')
+    countries_iso3 = StringListFilter(method='filter_countries_iso3')
+    hazard_sub_types = IDListFilter(method='filter_hazard_sub_types')
+    hazard_sub_categories = IDListFilter(method='filter_hazard_sub_categories')
+
+    class Meta:
+        model = DisplacementData
+        fields = ()
+
+    def filter_start_year(self, queryset, name, value):
+        return queryset.filter(year__gte=value)
+
+    def filter_end_year(self, queryset, name, value):
+        return queryset.filter(year__lte=value)
+
+    def filter_countries_iso3(self, queryset, name, value):
+        return queryset.filter(iso3__in=value)
+
+    def filter_hazard_sub_types(self, queryset, name, value):
+        return queryset.filter(country__country_disaster__hazard_sub_type__in=value)
+
+    def filter_hazard_sub_categories(self, queryset, name, value):
+        return queryset.filter(country__country_disaster__hazard_sub_category__in=value)
