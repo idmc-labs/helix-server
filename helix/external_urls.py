f847761e41bbd434cae5e82a60c06d93a0d52d4f
Bimal Prasad Pandey <pandeybi125@gmail.com>
2023-04-27 12:13:26 +0545

    Add gidd prefix in external urls

--- helix/external_urls.py
+++ helix/external_urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('idus', IdusFlatCachedView.as_view()),
     path('idus-all', IdusAllFlatCachedView.as_view()),
     path('idus-all-disaster', IdusAllDisasterCachedView.as_view()),
-    path('', include(router.urls)),
+    path('gidd', include(router.urls)),
 ]
