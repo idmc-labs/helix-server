c5a2947fe7bcb9a9063ebb13d62b8c1ee1cceacb
Bimal Prasad Pandey <pandeybi125@gmail.com>
2023-04-28 11:39:10 +0545

    Use REST filter instead of manual

---
+++ apps/gidd/rest_filters.py
@@ -0,0 +1,44 @@
+import django_filters
+
+from .models import (
+    Conflict,
+    Disaster,
+)
+
+
+class RestConflictFilterSet(django_filters.FilterSet):
+    start_year = django_filters.NumberFilter(field_name='start_year', method='filter_start_year')
+    end_year = django_filters.NumberFilter(field_name='end_year', method='filter_end_year')
+
+    class Meta:
+        model = Conflict
+        fields = {
+            'id': ['iexact'],
+            'iso3': ['iexact'],
+            'year': ['iexact'],
+        }
+
+    def filter_start_year(self, queryset, name, value):
+        return queryset.filter(year__gte=value)
+
+    def filter_end_year(self, queryset, name, value):
+        return queryset.filter(year__lte=value)
+
+
+class RestDisasterFilterSet(django_filters.FilterSet):
+    start_year = django_filters.NumberFilter(field_name='start_year', method='filter_start_year')
+    end_year = django_filters.NumberFilter(field_name='end_year', method='filter_end_year')
+
+    class Meta:
+        model = Disaster
+        fields = {
+            'id': ['iexact'],
+            'iso3': ['in'],
+            'hazard_type': ['in'],
+        }
+
+    def filter_start_year(self, queryset, name, value):
+        return queryset.filter(year__gte=value)
+
+    def filter_end_year(self, queryset, name, value):
+        return queryset.filter(year__lte=value)
