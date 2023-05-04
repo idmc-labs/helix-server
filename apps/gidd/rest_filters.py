0dea4d1e39cf2c5f6552e1cdf6bd7299428f0abd
Bimal Prasad Pandey <pandeybi125@gmail.com>
2023-05-04 15:28:02 +0545

    Add filter for displacement data

--- apps/gidd/rest_filters.py
+++ apps/gidd/rest_filters.py
@@ -3,6 +3,7 @@ from .filters import ReleaseMetadataFilter
 from .models import (
     Conflict,
     Disaster,
+    DisplacementData,
 )
 
 
@@ -42,3 +43,32 @@ class RestDisasterFilterSet(ReleaseMetadataFilter):
 
     def filter_end_year(self, queryset, name, value):
         return queryset.filter(year__lte=value)
+
+
+class RestDisplacementDataFilterSet(ReleaseMetadataFilter):
+    start_year = django_filters.NumberFilter(field_name='start_year', method='filter_start_year')
+    end_year = django_filters.NumberFilter(field_name='end_year', method='filter_end_year')
+    is_conflict = django_filters.BooleanFilter(method='filter_is_conflict')
+    is_disaster = django_filters.BooleanFilter(method='filter_is_disaster')
+
+    class Meta:
+        model = DisplacementData
+        fields = ()
+
+    def filter_start_year(self, queryset, name, value):
+        return queryset.filter(year__gte=value)
+
+    def filter_end_year(self, queryset, name, value):
+        return queryset.filter(year__lte=value)
+
+    def filter_is_conflict(self, queryset, name, value):
+        return queryset.filter(
+            conflict_total_displacement__isnull=False,
+            conflict_new_displacement__isnull=False
+        )
+
+    def filter_is_disaster(self, queryset, name, value):
+        return queryset.filter(
+            disaster_total_displacement__isnull=False,
+            disaster_new_displacement__isnull=False
+        )
