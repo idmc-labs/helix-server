fe4a099a046de6d9c0187780b4440c46c04741d8
Bimal Prasad Pandey <pandeybi125@gmail.com>
2023-05-09 14:48:53 +0545

    Remove strtobool method

--- utils/common.py
+++ utils/common.py
@@ -155,20 +155,3 @@ def track_gidd(client_id, endpoint_type):
         endpoint_type,
         client_id,
     )
-
-
-def strtobool(val):
-    """Convert a string representation of truth to true (1) or false (0).
-    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
-    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
-    'val' is anything else.
-    """
-    if not val:
-        return
-    val = val.lower()
-    if val in ('y', 'yes', 't', 'true', 'on', '1'):
-        return 1
-    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
-        return 0
-    else:
-        raise ValueError("invalid truth value %r" % (val,))
