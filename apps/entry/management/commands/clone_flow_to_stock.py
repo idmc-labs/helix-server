0ea9094ae0b16cf031c45cef2e750556968bf959
tnagorra <weathermist@gmail.com>
2023-05-10 11:20:17 +0545

    Fix issues with the migration scripts
    - Make transactions atomic
    - Fix cloning of figures

--- apps/entry/management/commands/clone_flow_to_stock.py
+++ apps/entry/management/commands/clone_flow_to_stock.py
@@ -1,6 +1,9 @@
 import csv
 import logging
+import copy
+from uuid import uuid4
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
 from apps.entry.models import Figure
 
@@ -14,45 +17,56 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('figures')
 
+    @transaction.atomic
     def handle(self, *args, **kwargs):
         figures_file = kwargs['figures']
 
         with open(figures_file, 'r') as figures_csv_file:
             reader = csv.DictReader(figures_csv_file)
-
             ids = [figure['id'] for figure in reader]
 
-        figures_cloned_list = []
+        success = 0
         for figure in Figure.objects.filter(
             id__in=ids
         ):
-            figure.id = None
-            figure.old_id = None
-            year = int(figure.start_date.year)
-            end_date = f'{year}-12-31'
-            figure.end_date = end_date
-            figure.role = Figure.ROLE.RECOMMENDED
-            figure.save()
+            new_figure = copy.deepcopy(figure)
 
-            # Also clone disaggregatedage and osmname relations
+            new_figure.id = None
+            new_figure.old_id = None
+            new_figure.uuid = uuid4()
+            year = int(new_figure.start_date.year)
+            end_date = f'{year}-12-31'
+            new_figure.end_date = end_date
+            new_figure.role = Figure.ROLE.RECOMMENDED
+            new_figure.category = Figure.FIGURE_CATEGORY_TYPES.IDPS
+            new_figure.was_subfact = False
+            new_figure.approved_by = None
+            new_figure.approved_on = None
+            new_figure.review_status = Figure.FIGURE_REVIEW_STATUS.REVIEW_NOT_STARTED
+            new_figure.save()
+
             geo_locations_list = []
             for geo_location in figure.geo_locations.all():
                 geo_location.id = None
-                geo_location_obj = geo_location.save()
-                geo_locations_list.append(geo_location_obj)
-            figure.geo_locations.set(geo_locations_list)
+                geo_location.uuid = uuid4()
+                geo_location.save()
+                geo_locations_list.append(geo_location)
+            new_figure.geo_locations.set(geo_locations_list)
+
+            new_figure.sources.set(figure.sources.all())
+            new_figure.tags.set(figure.tags.all())
+            new_figure.context_of_violence.set(figure.context_of_violence.all())
 
             disaggregation_age_list = []
             for disaggregation_age in figure.disaggregation_age.all():
                 disaggregation_age.id = None
-                disaggregation_age = disaggregation_age.save()
+                disaggregation_age.uuid = uuid4()
+                disaggregation_age.save()
                 disaggregation_age_list.append(disaggregation_age)
-            figure.disaggregation_age.set(disaggregation_age_list)
+            new_figure.disaggregation_age.set(disaggregation_age_list)
 
-            figures_cloned_list.append(figure.id)
+            success += 1
+            print(f'Cloned flow figure {figure.id} as stock figure {new_figure.id}')
 
         # Make a list of new figures that were cloned (clear old_id when cloning or we are going to have a bad time)
-        print(
-            f'Cloned {len(figures_cloned_list)} figures',
-            figures_cloned_list,
-        )
+        print(f'Cloned {success} flow figures as stock figures')
