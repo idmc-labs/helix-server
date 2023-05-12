c6bb8db65a9adafe1a9f9e6dd9ad0855d2bf5dbf
Bimal Prasad Pandey <pandeybi125@gmail.com>
2023-05-12 16:33:23 +0545

    Setup swagger and track REST endpoints

--- helix/external_urls.py
+++ helix/external_urls.py
@@ -1,3 +1,7 @@
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from apps.entry.views import (
@@ -12,6 +16,19 @@ from apps.gidd.views import (
     DisplacementDataViewSet,
 )
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="GIDD Open API",
+        default_version='v1',
+        description="Public REST endpoints",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="dev@togglecorp.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 router = DefaultRouter()
 router.register("countries", CountryViewSet, "countries-view")
 router.register("conflicts", ConflictViewSet, "conflicts-view")
@@ -23,4 +40,5 @@ urlpatterns = [
     path('idus-all', IdusAllFlatCachedView.as_view()),
     path('idus-all-disaster', IdusAllDisasterCachedView.as_view()),
     path('gidd/', include(router.urls)),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
