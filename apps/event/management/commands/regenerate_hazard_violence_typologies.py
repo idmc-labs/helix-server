ca8a8cab3ef9d6a758cf507986903cdc05ea63ca
tnagorra <weathermist@gmail.com>
2023-10-02 14:34:24 +0545

    Log updated figures and events
    - Comment out the part to update events using csv

--- apps/event/management/commands/regenerate_hazard_violence_typologies.py
+++ apps/event/management/commands/regenerate_hazard_violence_typologies.py
@@ -1,8 +1,7 @@
-import csv
 from django.core.management.base import BaseCommand
 from django.db.models import Q, OuterRef, Subquery
 
-from apps.event.models import Event, DisasterSubType
+from apps.event.models import Event
 from apps.entry.models import Figure
 
 
@@ -10,6 +9,7 @@ class Command(BaseCommand):
     help = 'Fix hazard sub types from csv'
 
     def handle(self, *args, **options):
+        '''
         with open('new_hazard_sub_types.csv', 'r') as file:
             csvreader = csv.DictReader(file)
             for row in csvreader:
@@ -24,67 +24,76 @@ class Command(BaseCommand):
 
                 figure.disaster_sub_type = disaster_sub_type
                 figure.save()
+        '''
 
         # Regenerate event and figure disaster type
-        Event.objects.filter(
+        update = Event.objects.filter(
             Q(disaster_sub_type__isnull=False)
         ).update(
             disaster_type=Subquery(
                 Event.objects.filter(pk=OuterRef('pk')).values('disaster_sub_type__type')[:1]
             ),
         )
-        Figure.objects.filter(
+        print(f'Updated disaster type for {update} events')
+        update = Figure.objects.filter(
             Q(disaster_sub_type__isnull=False)
         ).update(
             disaster_type=Subquery(
                 Figure.objects.filter(pk=OuterRef('pk')).values('disaster_sub_type__type')[:1]
             ),
         )
+        print(f'Updated disaster type for {update} figures')
 
         # Regenerate event and figure disaster sub category
-        Event.objects.filter(
+        update = Event.objects.filter(
             Q(disaster_type__isnull=False)
         ).update(
             disaster_sub_category=Subquery(
                 Event.objects.filter(pk=OuterRef('pk')).values('disaster_type__disaster_sub_category')[:1]
             ),
         )
-        Figure.objects.filter(
+        print(f'Updated disaster sub-category for {update} events')
+        update = Figure.objects.filter(
             Q(disaster_type__isnull=False)
         ).update(
             disaster_sub_category=Subquery(
                 Figure.objects.filter(pk=OuterRef('pk')).values('disaster_type__disaster_sub_category')[:1]
             ),
         )
+        print(f'Updated disaster sub-category for {update} figures')
 
         # Regenerate event and figure disaster category
-        Event.objects.filter(
+        update = Event.objects.filter(
             Q(disaster_sub_category__isnull=False)
         ).update(
             disaster_category=Subquery(
                 Event.objects.filter(pk=OuterRef('pk')).values('disaster_sub_category__category')[:1]
             ),
         )
-        Figure.objects.filter(
+        print(f'Updated disaster category for {update} events')
+        update = Figure.objects.filter(
             Q(disaster_sub_category__isnull=False)
         ).update(
             disaster_category=Subquery(
                 Figure.objects.filter(pk=OuterRef('pk')).values('disaster_sub_category__category')[:1]
             ),
         )
+        print(f'Updated disaster category for {update} figures')
 
         # Regenerate figure and event violence from violence sub type
-        Figure.objects.filter(
+        update = Figure.objects.filter(
             violence_sub_type__isnull=False
         ).update(
             violence=Subquery(
                 Figure.objects.filter(pk=OuterRef('pk')).values('violence_sub_type__violence')[:1]
             )
         )
-        Event.objects.filter(
+        print(f'Updated violence type for {update} events')
+        update = Event.objects.filter(
             violence_sub_type__isnull=False
         ).update(
             violence=Subquery(
                 Event.objects.filter(pk=OuterRef('pk')).values('violence_sub_type__violence')[:1]
             )
         )
+        print(f'Updated violence type for {update} figures')
