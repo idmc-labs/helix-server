da1319c0afdba7f21e4d420be6fea2a24adac03f
thenav56 <ayernavin@gmail.com>
2023-12-28 11:09:26 +0545

    Add basic mock test for event update
    - Use deepcopy instead of copy
    - Replace json.loads with response.json()
    - Don't mutate setUp attributes
    - Fix existing error check logic

--- apps/entry/tests/test_figure_bulk.py
+++ apps/entry/tests/test_figure_bulk.py
@@ -1,6 +1,6 @@
-import json
-from copy import copy
+from copy import deepcopy as copy
 from uuid import uuid4
+from unittest.mock import patch, call
 
 from utils.tests import HelixGraphQLTestCase, create_user_with_role
 from utils.factories import (
@@ -24,6 +24,8 @@ def get_first_error_fields(errors):
     ]
 
 
+@patch('apps.entry.mutations.BulkUpdateFigureManager.add_event')
+@patch('apps.entry.mutations.BulkUpdateFigureManager.__exit__')
 class TestBulkFigureUpdate(HelixGraphQLTestCase):
     def setUp(self) -> None:
         self.country_1 = CountryFactory.create(iso2='JP', iso3='JPN')
@@ -99,7 +101,11 @@ class TestBulkFigureUpdate(HelixGraphQLTestCase):
         """
         self.force_login(self.editor)
 
-    def test_can_bulk_create_and_delete_figures(self):
+    def test_can_bulk_create_and_delete_figures(
+        self,
+        mock_bulk_update_figure_manager_exit,
+        mock_bulk_update_figure_manager_add_event,
+    ):
         figures = [
             {
                 "uuid": str(uuid4()),
@@ -161,6 +167,8 @@ class TestBulkFigureUpdate(HelixGraphQLTestCase):
         ]
 
         figure_ids = [self.f1.id, self.f2.id, self.f3.id]
+        mock_bulk_update_figure_manager_add_event.assert_not_called()
+        mock_bulk_update_figure_manager_exit.assert_not_called()
         response = self.query(
             self.figure_bulk_mutation,
             variables={
@@ -174,7 +182,11 @@ class TestBulkFigureUpdate(HelixGraphQLTestCase):
         self.assertResponseNoErrors(response)
         self.assertEqual(content_data['errors'], [None] * 3)
         self.assertEqual(len(content_data['result']), 3)
+        self.assertNotIn(None, content_data['result'])
         self.assertEqual(len(content_data['deletedResult']), len(figure_ids), content_data)
+        assert mock_bulk_update_figure_manager_add_event.call_count == 6
+        mock_bulk_update_figure_manager_add_event.assert_has_calls([call(self.event.id)])
+        mock_bulk_update_figure_manager_exit.assert_called_once()
 
         # Test bulk deleted
         self.assertEqual(Figure.objects.filter(id__in=figure_ids).count(), 0)
@@ -185,7 +197,11 @@ class TestBulkFigureUpdate(HelixGraphQLTestCase):
             self.assertEqual(created_figure['includeIdu'], True)
             self.assertEqual(created_figure['entry']['id'], str(self.entry.id))
 
-    def test_can_bulk_update_and_delete_figures(self):
+    def test_can_bulk_update_and_delete_figures(
+        self,
+        mock_bulk_update_figure_manager_exit,
+        mock_bulk_update_figure_manager_add_event,
+    ):
         figures = [
             {
                 "id": self.f1.id,
@@ -213,6 +229,8 @@ class TestBulkFigureUpdate(HelixGraphQLTestCase):
             },
         ]
         figure_ids = [self.f3.id]
+        mock_bulk_update_figure_manager_add_event.assert_not_called()
+        mock_bulk_update_figure_manager_exit.assert_not_called()
         response = self.query(
             self.figure_bulk_mutation,
             variables={
@@ -220,16 +238,19 @@ class TestBulkFigureUpdate(HelixGraphQLTestCase):
                 "delete_ids": figure_ids,
             },
         )
+        assert mock_bulk_update_figure_manager_add_event.call_count == 3
+        mock_bulk_update_figure_manager_add_event.assert_has_calls([call(self.event.id)])
+        mock_bulk_update_figure_manager_exit.assert_called_once()
 
         # Test bulk deleted
         self.assertEqual(Figure.objects.filter(id__in=figure_ids).count(), 0)
 
         # Test updated
-        content = json.loads(response.content)
-        content_data = content['data']['bulkUpdateFigures']
+        content_data = response.json()['data']['bulkUpdateFigures']
         self.assertResponseNoErrors(response)
         self.assertEqual(content_data['errors'], [None] * 2)
         self.assertEqual(len(content_data['result']), 2)
+        self.assertNotIn(None, content_data['result'])
         assert None not in content_data['result']
 
         # Check each item
@@ -238,28 +259,36 @@ class TestBulkFigureUpdate(HelixGraphQLTestCase):
             self.assertEqual(updated_figure['includeIdu'], False)
             self.assertEqual(updated_figure['entry']['id'], str(self.entry.id))
 
-    def test_household_size_validation(self):
+    def test_household_size_validation(
+        self,
+        mock_bulk_update_figure_manager_exit,
+        mock_bulk_update_figure_manager_add_event,
+    ):
         """
         reported <= disaggregationLocationCamp + disaggregationLocationNonCamp
         """
-        self.figure_item_input.update({
+        figure_item_input = copy(self.figure_item_input)
+        figure_item_input.update({
             "reported": 30,
             "disaggregationLocationCamp": 200,
             "disaggregationLocationNonCamp": 10,
         })
+        mock_bulk_update_figure_manager_add_event.assert_not_called()
+        mock_bulk_update_figure_manager_exit.assert_not_called()
         response = self.query(
             self.figure_bulk_mutation,
             variables={
-                "items": [self.figure_item_input],
+                "items": [figure_item_input],
                 "delete_ids": [],
             }
         )
-        content = json.loads(response.content)
-        content_data = content['data']['bulkUpdateFigures']
-        self.assertNotIn('disaggregationLocationCamp', content_data['errors'])
-        self.assertNotIn('disaggregationLocationNonCamp', content_data['errors'])
+        assert mock_bulk_update_figure_manager_add_event.call_count == 0
+        assert mock_bulk_update_figure_manager_exit.call_count == 1
+        content_data = response.json()['data']['bulkUpdateFigures']
+        self.assertIn('disaggregationLocationCamp', get_first_error_fields(content_data['errors']))
+        self.assertIn('disaggregationLocationNonCamp', get_first_error_fields(content_data['errors']))
 
-        self.figure_item_input.update({
+        figure_item_input.update({
             "reported": 300,
             "disaggregationLocationCamp": 200,
             "disaggregationLocationNonCamp": 100,
@@ -267,36 +296,51 @@ class TestBulkFigureUpdate(HelixGraphQLTestCase):
         response = self.query(
             self.figure_bulk_mutation,
             variables={
-                "items": [self.figure_item_input],
+                "items": [figure_item_input],
                 "delete_ids": [],
             }
         )
-        content = json.loads(response.content)
-        content_data = content['data']['bulkUpdateFigures']
+        assert mock_bulk_update_figure_manager_add_event.call_count == 1
+        mock_bulk_update_figure_manager_add_event.assert_has_calls([call(self.event.id)])
+        assert mock_bulk_update_figure_manager_exit.call_count == 2
+        content_data = response.json()['data']['bulkUpdateFigures']
 
-    def test_invalid_figures_household_size(self):
+    def test_invalid_figures_household_size(
+        self,
+        mock_bulk_update_figure_manager_exit,
+        mock_bulk_update_figure_manager_add_event,
+    ):
         """
         If unit is househod, household_size must be supplied.
         """
         self.f3.household_size = None
         self.f3.save()
 
-        self.figure_item_input.update({
+        figure_item_input = copy(self.figure_item_input)
+        figure_item_input.update({
             "unit": Figure.UNIT.HOUSEHOLD.name,  # missing household_size
         })
+        mock_bulk_update_figure_manager_add_event.assert_not_called()
+        mock_bulk_update_figure_manager_exit.assert_not_called()
         response = self.query(
             self.figure_bulk_mutation,
             variables={
-                "items": [self.figure_item_input],
+                "items": [figure_item_input],
                 "delete_ids": [],
             }
         )
-        content = json.loads(response.content)
-        content_data = content['data']['bulkUpdateFigures']
+        mock_bulk_update_figure_manager_add_event.assert_not_called()
+        mock_bulk_update_figure_manager_exit.assert_called_once()
+        content_data = response.json()['data']['bulkUpdateFigures']
         assert 'householdSize' in get_first_error_fields(content_data['errors'])
 
-    def test_invalid_figures_age_data(self):
-        self.figure_item_input.update({
+    def test_invalid_figures_age_data(
+        self,
+        mock_bulk_update_figure_manager_exit,
+        mock_bulk_update_figure_manager_add_event,
+    ):
+        figure_item_input = copy(self.figure_item_input)
+        figure_item_input.update({
             "disaggregationAge": [
                 # invalid: category and sex is duplicated
                 {
@@ -315,18 +359,22 @@ class TestBulkFigureUpdate(HelixGraphQLTestCase):
                 }
             ],
         })
+        mock_bulk_update_figure_manager_add_event.assert_not_called()
+        mock_bulk_update_figure_manager_exit.assert_not_called()
         response = self.query(
             self.figure_bulk_mutation,
             variables={
-                "items": [self.figure_item_input],
+                "items": [figure_item_input],
                 "delete_ids": [],
             }
         )
+        mock_bulk_update_figure_manager_add_event.assert_not_called()
+        mock_bulk_update_figure_manager_exit.assert_called_once()
         content_data = response.json()['data']['bulkUpdateFigures']
         assert content_data['result'] == [None]
         assert 'disaggregationAge' in get_first_error_fields(content_data['errors'])
 
-    def test_figure_cause_should_be_same_as_event_type(self):
+    def test_figure_cause_should_be_same_as_event_type(self, *_):
         event_1 = EventFactory.create(event_type=Crisis.CRISIS_TYPE.CONFLICT)
         event_2 = EventFactory.create(event_type=Crisis.CRISIS_TYPE.DISASTER)
         event_3 = EventFactory.create(event_type=Crisis.CRISIS_TYPE.OTHER)
@@ -356,8 +404,7 @@ class TestBulkFigureUpdate(HelixGraphQLTestCase):
                 "delete_ids": [],
             }
         )
-        content = json.loads(response.content)
-        content_data = content['data']['bulkUpdateFigures']
+        content_data = response.json()['data']['bulkUpdateFigures']
         self.assertResponseNoErrors(response)
         assert 'figureCause' in get_first_error_fields(content_data['errors'])
 
@@ -381,52 +428,55 @@ class TestBulkFigureUpdate(HelixGraphQLTestCase):
                 "delete_ids": [],
             }
         )
-        content = json.loads(response.content)
-        content_data = content['data']['bulkUpdateFigures']
+        content_data = response.json()['data']['bulkUpdateFigures']
         self.assertResponseNoErrors(response)
         assert 'figureCause' not in get_first_error_fields(content_data['errors'])
 
-    def test_figure_include_idu_validation(self):
+    def test_figure_include_idu_validation(self, *_):
         """
         If includeIdu is True, excerptIdu must be provided.
         """
         # Pass invalid input and test
-        self.figure_item_input.update({
+        figure_item_input = copy(self.figure_item_input)
+        figure_item_input.update({
             'includeIdu': True, 'excerptIdu': '  ',
         })
         response = self.query(
             self.figure_bulk_mutation,
             variables={
-                "items": [self.figure_item_input],
+                "items": [figure_item_input],
                 "delete_ids": [],
             }
         )
-        content = json.loads(response.content)
-        content_data = content['data']['bulkUpdateFigures']
+        content_data = response.json()['data']['bulkUpdateFigures']
         assert 'excerptIdu' in get_first_error_fields(content_data['errors'])
 
         # Pass correct value and test
-        self.figure_item_input.update({
+        figure_item_input.update({
             'includeIdu': False, 'excerptIdu': '  ',
         })
         response = self.query(
             self.figure_bulk_mutation,
             variables={
-                "items": [self.figure_item_input],
+                "items": [figure_item_input],
                 "delete_ids": [],
             }
         )
-        content = json.loads(response.content)
-        content_data = content['data']['bulkUpdateFigures']
+        content_data = response.json()['data']['bulkUpdateFigures']
         assert 'excerptIdu' not in get_first_error_fields(content_data['errors'])
 
-    def test_should_not_update_event_in_figure(self):
+    def test_should_not_update_event_in_figure(
+        self,
+        mock_bulk_update_figure_manager_exit,
+        mock_bulk_update_figure_manager_add_event,
+    ):
         entry = EntryFactory.create()
-        event1 = EventFactory.create()
-        event2 = EventFactory.create()
-        event3 = EventFactory.create()
+        event1, event2, event3 = EventFactory.create_batch(3)
         figure1 = FigureFactory.create(entry=entry, event=event1)
         figure2 = FigureFactory.create(entry=entry, event=event2)
+
+        for _event in [event1, event2, event3]:
+            _event.countries.add(self.country_1, self.country_2)
 
         # Make copies of input
         figure_input_1 = copy(self.figure_item_input)
@@ -434,10 +484,12 @@ class TestBulkFigureUpdate(HelixGraphQLTestCase):
 
         # Test with correct event ids
         figure_input_1.update({
-            'id': figure1.id, 'event': event1.id,
+            'id': figure1.id,
+            'event': event1.id,
         })
         figure_input_2.update({
-            'id': figure2.id, 'event': event2.id,
+            'id': figure2.id,
+            'event': event2.id,
         })
         response = self.query(
             self.figure_bulk_mutation,
@@ -446,17 +498,25 @@ class TestBulkFigureUpdate(HelixGraphQLTestCase):
                 "delete_ids": [],
             }
         )
+        assert mock_bulk_update_figure_manager_add_event.call_count == 2
+        mock_bulk_update_figure_manager_add_event.assert_has_calls([
+            call(event1.id),
+            call(event2.id),
+        ], any_order=True)
+        mock_bulk_update_figure_manager_exit.assert_called_once()
         self.assertResponseNoErrors(response)
-        content = json.loads(response.content)
-        content_data = content['data']['bulkUpdateFigures']
-        self.assertNotIn('event', content_data['errors'][0])
+        content_data = response.json()['data']['bulkUpdateFigures']
+        self.assertNotIn('event', get_first_error_fields(content_data['errors']))
+        self.assertNotEqual(content_data['result'], [None, None])
 
         # Test with incorrect event ids
         figure_input_1.update({
-            'id': figure1.id, 'event': event1.id,
+            'id': figure1.id,
+            'event': event1.id,
         })
         figure_input_2.update({
-            'id': figure2.id, 'event': event3.id,
+            'id': figure2.id,
+            'event': event3.id,
         })
         response = self.query(
             self.figure_bulk_mutation,
@@ -465,7 +525,12 @@ class TestBulkFigureUpdate(HelixGraphQLTestCase):
                 "delete_ids": [],
             }
         )
+        assert mock_bulk_update_figure_manager_add_event.call_count == 3
+        mock_bulk_update_figure_manager_add_event.assert_has_calls([
+            call(event1.id),
+            call(event2.id),
+        ])
+        assert mock_bulk_update_figure_manager_exit.call_count == 2
         self.assertResponseNoErrors(response)
-        content = json.loads(response.content)
-        content_data = content['data']['bulkUpdateFigures']
-        self.assertNotIn('event', content_data['errors'][0])
+        content_data = response.json()['data']['bulkUpdateFigures']
+        self.assertIn('event', get_first_error_fields(content_data['errors']))
