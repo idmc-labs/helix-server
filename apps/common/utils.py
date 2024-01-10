a68ea357966a603a5122523ce48bd6dc05b7e534
tnagorra <weathermist@gmail.com>
2024-01-10 20:41:04 +0545

    Fix event code issue on GIDD generation
    - Use typing for event code utility

--- apps/common/utils.py
+++ apps/common/utils.py
@@ -1,26 +1,44 @@
+from typing import Literal
+
+TUPLE_SEPARATOR = ', '
 ARRAY_SEPARATOR = '; '
 FIELD_SEPARATOR = ':'
-TUPLE_SEPARATOR = ', '
 
 
-def get_event_code(event_codes, type=None):
+# FIXME: rename this to get_attr_list_from_event_codes
+def get_event_code_gidd(
+    event_codes: str,
+    type: Literal['code', 'code_type', 'iso3'],
+):
     from apps.event.models import EventCode
 
-    def _get_event_code_label(key):
+    def _get_event_code_label(key: int) -> str:
         obj = EventCode.EVENT_CODE_TYPE.get(key)
+        # NOTE: Why are we using int type for key
         return getattr(obj, "label", key)
+
     if not event_codes or event_codes == '':
-        return
+        return []
 
-    # FIXME: We also get aggregation when there is not data
-    splitted_event_codes = [event_code.split(FIELD_SEPARATOR) for event_code in event_codes
-                            if event_code != f'{FIELD_SEPARATOR}{FIELD_SEPARATOR}']
+    # NOTE: We also get aggregation when there is not data
+    # so we also check for '::'
+    splitted_event_codes = [
+        event_code.split(FIELD_SEPARATOR)
+        for event_code in event_codes
+        if event_code != f'{FIELD_SEPARATOR}{FIELD_SEPARATOR}'
+    ]
 
-    # TODO: get country as well
-
-    return ARRAY_SEPARATOR.join([
+    return [
         event_code[0] if type == 'code'
-        else _get_event_code_label(int(event_code[1])
-                                   if type == 'code_type' else event_code[2])
+        else _get_event_code_label(int(event_code[1])) if type == 'code_type'
+        else event_code[2]
         for event_code in splitted_event_codes
-    ])
+    ]
+
+
+# FIXME: rename this to get_attr_str_from_event_codes
+def get_event_code(
+    event_codes: str,
+    type: Literal['code', 'code_type', 'iso3'],
+):
+    return ARRAY_SEPARATOR.join(get_event_code_gidd(event_codes, type))
