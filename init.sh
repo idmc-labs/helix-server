13dd8e2a567819c35c04e1f187d6a8f22c748fe3
thenav56 <ayernavin@gmail.com>
2025-01-28 15:17:01 +0545

    Fix docker-compose deprecation in GH

--- init.sh
+++ init.sh
@@ -1,18 +1,18 @@
 #! /bin/bash -xe
 
 # Run migrations
-docker-compose exec server python manage.py migrate
+docker compose exec server python manage.py migrate
 
 # No need to collect static init.sh
 
 # Init roles
-docker-compose exec server python manage.py remove_stale_contenttypes
-docker-compose exec server python manage.py init_roles
+docker compose exec server python manage.py remove_stale_contenttypes
+docker compose exec server python manage.py init_roles
 
 # Init assets
-docker-compose exec server python manage.py init_types_subtypes # event related
-docker-compose exec server python manage.py init_figure_tags
-docker-compose exec server python manage.py init_osv_sub_type
-docker-compose exec server python manage.py init_other_sub_types
-docker-compose exec server python manage.py init_context_of_violences
-docker-compose exec server python manage.py save_users
+docker compose exec server python manage.py init_types_subtypes # event related
+docker compose exec server python manage.py init_figure_tags
+docker compose exec server python manage.py init_osv_sub_type
+docker compose exec server python manage.py init_other_sub_types
+docker compose exec server python manage.py init_context_of_violences
+docker compose exec server python manage.py save_users
