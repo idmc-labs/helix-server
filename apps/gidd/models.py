5960424ef779e6103bf0766da0c74e80ae10a2d1
thenav56 <ayernavin@gmail.com>
2025-01-29 17:11:38 +0545

    Use last success status log for updated_at

--- apps/gidd/models.py
+++ apps/gidd/models.py
@@ -160,7 +160,7 @@ class StatusLog(models.Model):
 
     @classmethod
     def last_release_date(cls, format=None) -> typing.Optional[str]:
-        last_log = StatusLog.objects.last()
+        last_log = StatusLog.objects.filter(status=cls.Status.SUCCESS).last()
         if last_log:
             _format = format or "%B %d, %Y"
             return last_log.completed_at.strftime(_format)
