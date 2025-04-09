0f57427998c8f6de62897908c01c7c01c3172739
tnagorra <weathermist@gmail.com>
2025-04-09 09:41:44 +0545

    Fix issue with figure migration check
    - Refactor migration scripts

--- apps/entry/migrations/0105_migrate_figures_date.py
+++ apps/entry/migrations/0105_migrate_figures_date.py
@@ -5,7 +5,6 @@ import logging
 from datetime import datetime, timedelta
 
 from django.db import migrations
-from django.conf import settings
 
 from helix.managers import BulkUpdateManager
 from apps.entry.models import Figure as FigureModel
@@ -21,24 +20,23 @@ def update_figure_dates(apps, _):
         'correct_figures_dates.csv.gz',
     )
 
-    Figure = apps.get_model('entry', 'Figure')
     with gzip.open(file_path, 'rt', encoding='utf-8') as fp:
         csv_reader = csv.DictReader(fp, fieldnames=['type', 'id', 'old_id', 'start_date', 'end_date'])
+        # Skip the csv header
+        next(csv_reader)
 
-        next(csv_reader)  # Skip headers
         for row in csv_reader:
             figure_queryset = Figure.objects.filter(old_id=row['old_id'])
 
             # NOTE: Check if figure exist or not
             if not figure_queryset.exists():
-                logger.error(
-                    f"For the figure old id {row['old_id']}, no figures found"
-                )
+                logger.error(f"Skipped: Figure ({row['old_id']}) not found.")
                 continue
 
             figure_count = figure_queryset.count()
 
-            # NOTE: Checking for the figures containing same old_id
+            # NOTE: We should can get 2 figures with the same old_id in cases of
+            # partially added figures in Helix 1.0
             if figure_count == 2:
                 flow_figures_count = 0
                 stock_figures_count = 0
@@ -49,18 +47,15 @@ def update_figure_dates(apps, _):
                         flow_figures_count += 1
 
                 if flow_figures_count == 1 and stock_figures_count == 1:
-                    logger.error(
-                        f"For the figure id {figure_instance.id}, Got {flow_figures_count} flows and {stock_figures_count} stocks"
-                    )
+                    logger.error(f"Skipped: Figure ({row['old_id']} has 2 figures with different categories.")
                     continue
             elif figure_count > 2:
-                logger.error(
-                    f"We expected only 2 figures but got {figure_count}"
-                )
+                logger.error(f"Skipped: Figure ({row['old_id']} has {figure_count} figures.")
                 continue
 
             for figure_instance in figure_queryset.iterator():
                 update_needed = False
+
                 if row['start_date']:
                     correct_start_date = datetime.strptime(row['start_date'], '%Y-%m-%d').date()
                     if correct_start_date == figure_instance.start_date:
@@ -70,33 +65,25 @@ def update_figure_dates(apps, _):
                         figure_instance.start_date = correct_start_date
                         update_needed = True
                     else:
-                        logger.warning(
-                            f"The difference between the actual start date and the correct start date is not 1 day for figure old id {row['old_id']}. "
-                            f"Expected {row['start_date']} but found {figure_instance.start_date}. Skipping..."
-                        )
+                        logger.warning(f"Flag: For figure ({row['old_id']}), delta between actual start_date ({figure_instance.start_date}) and the correct start_date ({row['start_date']}) is greater than 1")
 
-                # Update End date                
                 if row['end_date']:
                     correct_end_date = datetime.strptime(row['end_date'], '%Y-%m-%d').date()
                     if correct_end_date == figure_instance.end_date:
                         # NOTE: No need to migrate the end_date
                         pass
-                    elif abs(figure_instance.end_date - correct_end_date) == timedelta(days=1) and figure_instance.start_date <= correct_end_date:
-                        # NOTE: Check if the correct end date is before the start date
+                    elif abs(figure_instance.end_date - correct_end_date) == timedelta(days=1):
                         figure_instance.end_date = correct_end_date
                         update_needed = True
                     else:
-                        logger.warning(
-                            f"The difference between the actual end date and the correct end date is not 1 day for figure old id {row['old_id']}. "
-                            f"Expected {row['end_date']} but found {figure_instance.end_date}. Skipping..."
-                        )
+                        logger.warning(f"Flag: For figure ({row['old_id']}), delta between actual end_date ({figure_instance.end_date}) and the correct end_date ({row['end_date']}) is greater than 1")
 
-                    # NOTE: Logging if the start date is greater than the end date
-                    if figure_instance.start_date > correct_end_date:
-                        logger.error(
-                            f"The start date is greater than the end date for figure old id {row['old_id']}. "
-                            f"Start date: {figure_instance.start_date}, End date: {correct_end_date}. Skipping..."
-                        )
+
+                # NOTE: Logging if the start date is greater than the end date
+                if figure_instance.start_date and figure_instance.end_date and figure_instance.start_date > figure_instance.end_date:
+                    logger.warning(f"Flag: For figure ({row['old_id']}), start_date ({figure_instance.start_date}) is greater than end_date ({figure_instance.end_date})")
+                    continue
+
                 if update_needed:
                     bulk_mgr.add(figure_instance)
 
