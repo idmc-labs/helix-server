4076cc239885ad154ac4ac896ca5f91a7545cdb1
Sushil Tiwari <susiltiwari750@gmail.com>
2025-05-02 17:37:35 +0545

    refactor(entry): Add bulkmgr and Update migration script for source file

--- apps/entry/management/commands/migrate_source_files.py
+++ apps/entry/management/commands/migrate_source_files.py
@@ -8,6 +8,7 @@ from django.db import transaction
 
 from apps.contrib.models import Attachment, SourcePreview
 from apps.entry.models import Entry
+from helix.managers import BulkUpdateManager
 
 
 class Command(BaseCommand):
@@ -17,15 +18,20 @@ class Command(BaseCommand):
     Everything else should be considered as a Document type entry
     """
 
+    def __init__(self) -> None:
+        super().__init__()
+        self.magic_type = magic.Magic()
+        self.magic_encoding = magic.Magic(flags=magic.MAGIC_MIME_ENCODING)
+
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument("csv_file_path", type=str, help="Path to the CSV file containing the data.")
 
-    def convert_entry_to_document_type(self, entry: Entry):
+    def convert_entry_to_document_type(self, entry: Entry, bulk_mgr: BulkUpdateManager):
         """Create a document attachment and detach the preview from the entry."""
         with entry.preview.pdf.open("rb") as file:
-            byte_stream = file.read()
-            filetype_detail = magic.Magic().id_buffer(byte_stream)
-            encoding = magic.Magic(flags=magic.MAGIC_MIME_ENCODING).id_buffer(byte_stream)
+            byte_stream = file.read(2048)
+            filetype_detail = self.magic_type.id_buffer(byte_stream)
+            encoding = self.magic_encoding.id_buffer(byte_stream)
 
         entry.document = Attachment.objects.create(
             attachment_for=Attachment.FOR_CHOICES.ENTRY,
@@ -37,16 +43,9 @@ class Command(BaseCommand):
         entry.document_url = entry.url
         entry.url = None
         entry.preview = None
-        entry.save(
-            update_fields=[
-                "document",
-                "document_url",
-                "url",
-                "preview",
-            ]
-        )
+        bulk_mgr.add(entry)
 
-    def convert_entry_to_url_type(self, entry: Entry):
+    def convert_entry_to_url_type(self, entry: Entry, bulk_mgr: BulkUpdateManager):
         """Create a SourcePreview and detach the document from the entry."""
         entry.preview = SourcePreview.objects.create(
             url=entry.document_url,
@@ -56,14 +55,7 @@ class Command(BaseCommand):
         entry.url = entry.document_url
         entry.document_url = None
         entry.document = None
-        entry.save(
-            update_fields=[
-                "preview",
-                "url",
-                "document",
-                "document_url",
-            ]
-        )
+        bulk_mgr.add(entry)
 
     @transaction.atomic
     def handle(self, *args: typing.Any, **kwargs: typing.Any):
@@ -90,15 +82,16 @@ class Command(BaseCommand):
         # NOTE: entry with previews and documents should be zero
         assert entry_with_previews_from_helix1.filter(document__isnull=False).count() == 0
 
-        for entry in entry_with_previews_from_helix1:
+        bulk_mgr = BulkUpdateManager(["document", "document_url", "url", "preview"], chunk_size=1000)
+        for entry in entry_with_previews_from_helix1.iterator():
             filename = entry.preview.pdf.name.split("helix-old/")[1]
             metadata = mapping.get(filename)
             if not metadata:
                 self.stdout.write(self.style.ERROR(f"Metadata not found for Entry ({entry.id}) with filename ({filename})"))
                 continue
             if metadata["type"] == "document":
-                self.convert_entry_to_document_type(entry=entry)
-                self.stdout.write(self.style.SUCCESS(f"Converted entry {entry.id} to document type."))
+                self.convert_entry_to_document_type(entry=entry, bulk_mgr=bulk_mgr)
+                self.stdout.write(self.style.SUCCESS(f"Converting entry {entry.id} to document type."))
                 updated_documents_count += 1
 
         # NOTE: We should check if the filepath starts with helix-old/ because it might have changed
@@ -109,16 +102,18 @@ class Command(BaseCommand):
         # NOTE: entry with documents and previews should be zero
         assert entry_with_documents_from_helix1.filter(preview__isnull=False).count() == 0
 
-        for entry in entry_with_documents_from_helix1:
+        for entry in entry_with_documents_from_helix1.iterator():
             filename = entry.document.attachment.name.split("helix-old/")[1]
             metadata = mapping.get(filename)
             if not metadata:
                 self.stdout.write(self.style.ERROR(f"Metadata not found for Entry ({entry.id}) with filename ({filename})"))
                 continue
             if metadata["type"] == "url" and entry.document_url:
-                self.convert_entry_to_url_type(entry=entry)
-                self.stdout.write(self.style.SUCCESS(f"Converted entry {entry.id} to url type."))
+                self.convert_entry_to_url_type(entry=entry, bulk_mgr=bulk_mgr)
+                self.stdout.write(self.style.SUCCESS(f"Converting entry {entry.id} to url type."))
                 updated_previews_count += 1
 
+        bulk_mgr.done()
+        self.stdout.write(self.style.SUCCESS(f"Successfully updated {bulk_mgr.summary()} entries."))
         self.stdout.write(self.style.SUCCESS(f"Updated {updated_documents_count} entries to document type."))
         self.stdout.write(self.style.SUCCESS(f"Updated {updated_previews_count} entries to url type."))
