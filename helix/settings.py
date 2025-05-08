85dbf6edd3839ef16147015b2278d2f19c1c9f92
tnagorra <weathermist@gmail.com>
2025-05-08 18:45:37 +0545

    Disable sorting of operation parameters

--- helix/settings.py
+++ helix/settings.py
@@ -686,6 +686,8 @@ SPECTACULAR_SETTINGS = {
     'SWAGGER_UI_DIST': 'SIDECAR',  # shorthand to use the sidecar instead
     'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
     'REDOC_DIST': 'SIDECAR',
+
+    'SORT_OPERATION_PARAMETERS': False,
 }
 
 if DEBUG:
