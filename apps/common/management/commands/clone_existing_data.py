a4caaa845af59c001b5249e8d67e18923d3e953b
tnagorra <weathermist@gmail.com>
2026-01-31 13:05:00 +0545

    fix(double-data): populate all m2m fields

--- apps/common/management/commands/clone_existing_data.py
+++ apps/common/management/commands/clone_existing_data.py
@@ -11,6 +11,7 @@ from apps.crisis.models import Crisis
 from apps.entry.models import Entry, Figure, FigureLocation
 from apps.event.models import Event
 from apps.report.models import Report
+from utils.common import RuntimeProfile
 
 logger = logging.getLogger(__name__)
 
@@ -61,22 +62,33 @@ def clone_crisis():
             new_crisis.start_date -= relativedelta(years=YEAR_DIFF)
             new_crisis.end_date -= relativedelta(years=YEAR_DIFF)
 
+    def post_mutate_crisis(new_crisis, old_crisis):
+        old_country_ids = old_crisis.countries.values_list("id", flat=True)
+        new_crisis.countries.set(old_country_ids)
+
     crisis_qs = Crisis.objects.iterator(chunk_size=1000)
-    old_to_new_crisis_map = clone_data(Crisis, crisis_qs, mutate_new_crisis)
+    old_to_new_crisis_map = clone_data(Crisis, crisis_qs, mutate_new_crisis, post_mutate_crisis)
 
     return old_to_new_crisis_map
 
 
 def clone_event(old_to_new_crisis_map):
     def mutate_new_event(new_event, old_event):
-        # FIXME: Currently, we are not duplicating event codes.
         new_event.start_date -= relativedelta(years=YEAR_DIFF)
         new_event.end_date -= relativedelta(years=YEAR_DIFF)
         # NOTE: Attach new crisis to new event
         new_event.crisis_id = old_to_new_crisis_map.get(new_event.crisis_id)
 
+    def post_mutate_event(new_event, old_event):
+        # FIXME: Currently, we are not duplicating event codes.
+        old_country_ids = old_event.countries.values_list("id", flat=True)
+        new_event.countries.set(old_country_ids)
+
+        old_context_of_violence_ids = old_event.context_of_violence.values_list("id", flat=True)
+        new_event.context_of_violence.set(old_context_of_violence_ids)
+
     event_queryset = Event.objects.iterator(chunk_size=1000)
-    old_to_new_event_map = clone_data(Event, event_queryset, mutate_new_event)
+    old_to_new_event_map = clone_data(Event, event_queryset, mutate_new_event, post_mutate_event)
 
     return old_to_new_event_map
 
@@ -110,8 +122,15 @@ def clone_entry(old_to_new_preview_map, old_to_new_attachment_map):
         else:
             new_entry.document_id = None
 
+    def post_mutate_entry(new_entry, old_entry):
+        old_publisher_ids = old_entry.publishers.values_list("id", flat=True)
+        new_entry.publishers.set(old_publisher_ids)
+
+        old_reviewer_ids = old_entry.reviewers.values_list("id", flat=True)
+        new_entry.reviewers.set(old_reviewer_ids)
+
     entry_queryset = Entry.objects.iterator(chunk_size=1000)
-    old_to_new_entry_map = clone_data(Entry, entry_queryset, mutate_entry)
+    old_to_new_entry_map = clone_data(Entry, entry_queryset, mutate_entry, post_mutate_entry)
 
     return old_to_new_entry_map
 
@@ -139,9 +158,19 @@ def clone_figure(
         new_figure_obj.event_id = old_to_new_event_map.get(old_event_id)
 
     def post_mutate_figure(new_figure, old_figure):
+        # FIXME: Currently, we are not cloning disaggregation_age
         old_location_ids = old_figure.geo_locations.values_list("id", flat=True)
         new_location_ids = [old_to_new_location_map.get(old_location_id) for old_location_id in old_location_ids]
         new_figure.geo_locations.set(new_location_ids)
+
+        old_tag_ids = old_figure.tags.values_list("id", flat=True)
+        new_figure.tags.set(old_tag_ids)
+
+        old_context_of_violence_ids = old_figure.context_of_violence.values_list("id", flat=True)
+        new_figure.context_of_violence.set(old_context_of_violence_ids)
+
+        old_source_ids = old_figure.sources.values_list("id", flat=True)
+        new_figure.sources.set(old_source_ids)
 
     figure_qs = Figure.objects.iterator(chunk_size=1000)
     clone_data(Figure, figure_qs, mutate_figure, post_mutate_figure)
@@ -172,34 +201,34 @@ class Command(BaseCommand):
             logger.warning("ENABLE_DANGER_MODE needs to be enabled to use this command")
             return
 
-        # Source preview
-        old_to_new_preview_map = clone_source_preview()
+        with RuntimeProfile("clone_source_preview"):
+            old_to_new_preview_map = clone_source_preview()
 
-        # Document
-        old_to_new_attachment_map = clone_attachment()
+        with RuntimeProfile("clone_attachment"):
+            old_to_new_attachment_map = clone_attachment()
 
-        # Entry
-        old_to_new_entry_map = clone_entry(old_to_new_preview_map, old_to_new_attachment_map)
-        del old_to_new_preview_map
-        del old_to_new_attachment_map
+        with RuntimeProfile("clone_entry"):
+            old_to_new_entry_map = clone_entry(old_to_new_preview_map, old_to_new_attachment_map)
+            del old_to_new_preview_map
+            del old_to_new_attachment_map
 
-        # Crisis
-        old_to_new_crisis_map = clone_crisis()
+        with RuntimeProfile("clone_crisis"):
+            old_to_new_crisis_map = clone_crisis()
 
-        # Event
-        old_to_new_event_map = clone_event(old_to_new_crisis_map)
-        del old_to_new_crisis_map
+        with RuntimeProfile("clone_event"):
+            old_to_new_event_map = clone_event(old_to_new_crisis_map)
+            del old_to_new_crisis_map
 
-        # Figure Location
-        old_to_new_location_map = clone_figure_location()
+        with RuntimeProfile("clone_location"):
+            old_to_new_location_map = clone_figure_location()
 
-        # Figure
-        clone_figure(old_to_new_entry_map, old_to_new_location_map, old_to_new_event_map)
-        del old_to_new_entry_map
-        del old_to_new_location_map
-        del old_to_new_event_map
+        with RuntimeProfile("clone_figure"):
+            clone_figure(old_to_new_entry_map, old_to_new_location_map, old_to_new_event_map)
+            del old_to_new_entry_map
+            del old_to_new_location_map
+            del old_to_new_event_map
 
-        # Report
-        clone_report()
+        with RuntimeProfile("clone_report"):
+            clone_report()
 
         logger.info("Successfully cloned data!")
