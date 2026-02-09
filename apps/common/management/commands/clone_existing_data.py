99aab9bc288a3e5b43386ca2326a70c00363d318
rup-narayan-rajbanshi <rup.rajbanshi@togglecorp.com>
2026-02-09 17:26:19 +0545

    chore(double-data): use generic func for cloning many2many field.

--- apps/common/management/commands/clone_existing_data.py
+++ apps/common/management/commands/clone_existing_data.py
@@ -19,6 +19,19 @@ logger = logging.getLogger(__name__)
 YEAR_DIFF = 9
 
 
+def get_many_to_many_field_names(model):
+    return [
+        field.name
+        for field in model._meta.get_fields()
+        if isinstance(field, models.ManyToManyField) and not field.auto_created
+    ]
+
+
+def clone_many_to_many_field(new_object, old_object, field_name):
+    old_ids = getattr(old_object, field_name).values_list("id", flat=True)
+    getattr(new_object, field_name).set(old_ids)
+
+
 def clone_data(Model, queryset, mutate=None, post_mutate=None):
     logger.info(f"Copying {Model.__name__} data")
 
@@ -63,8 +76,8 @@ def clone_crisis():
             new_crisis.end_date -= relativedelta(years=YEAR_DIFF)
 
     def post_mutate_crisis(new_crisis, old_crisis):
-        old_country_ids = old_crisis.countries.values_list("id", flat=True)
-        new_crisis.countries.set(old_country_ids)
+        for field in get_many_to_many_field_names(Crisis):
+            clone_many_to_many_field(new_crisis, old_crisis, field)
 
     crisis_qs = Crisis.objects.iterator(chunk_size=1000)
     old_to_new_crisis_map = clone_data(Crisis, crisis_qs, mutate_new_crisis, post_mutate_crisis)
@@ -81,11 +94,8 @@ def clone_event(old_to_new_crisis_map):
 
     def post_mutate_event(new_event, old_event):
         # FIXME: Currently, we are not duplicating event codes.
-        old_country_ids = old_event.countries.values_list("id", flat=True)
-        new_event.countries.set(old_country_ids)
-
-        old_context_of_violence_ids = old_event.context_of_violence.values_list("id", flat=True)
-        new_event.context_of_violence.set(old_context_of_violence_ids)
+        for field in get_many_to_many_field_names(Event):
+            clone_many_to_many_field(new_event, old_event, field)
 
     event_queryset = Event.objects.iterator(chunk_size=1000)
     old_to_new_event_map = clone_data(Event, event_queryset, mutate_new_event, post_mutate_event)
@@ -123,11 +133,8 @@ def clone_entry(old_to_new_preview_map, old_to_new_attachment_map):
             new_entry.document_id = None
 
     def post_mutate_entry(new_entry, old_entry):
-        old_publisher_ids = old_entry.publishers.values_list("id", flat=True)
-        new_entry.publishers.set(old_publisher_ids)
-
-        old_reviewer_ids = old_entry.reviewers.values_list("id", flat=True)
-        new_entry.reviewers.set(old_reviewer_ids)
+        for field in get_many_to_many_field_names(Entry):
+            clone_many_to_many_field(new_entry, old_entry, field)
 
     entry_queryset = Entry.objects.iterator(chunk_size=1000)
     old_to_new_entry_map = clone_data(Entry, entry_queryset, mutate_entry, post_mutate_entry)
@@ -163,31 +170,15 @@ def clone_figure(
         new_location_ids = [old_to_new_location_map.get(old_location_id) for old_location_id in old_location_ids]
         new_figure.geo_locations.set(new_location_ids)
 
-        old_tag_ids = old_figure.tags.values_list("id", flat=True)
-        new_figure.tags.set(old_tag_ids)
-
-        old_context_of_violence_ids = old_figure.context_of_violence.values_list("id", flat=True)
-        new_figure.context_of_violence.set(old_context_of_violence_ids)
-
-        old_source_ids = old_figure.sources.values_list("id", flat=True)
-        new_figure.sources.set(old_source_ids)
+        for field in get_many_to_many_field_names(Figure):
+            if not field == "geo_locations":
+                clone_many_to_many_field(new_figure, old_figure, field)
 
     figure_qs = Figure.objects.iterator(chunk_size=1000)
     clone_data(Figure, figure_qs, mutate_figure, post_mutate_figure)
 
 
 def clone_report():
-    def get_many_to_many_field_names(model):
-        return [
-            field.name
-            for field in model._meta.get_fields()
-            if isinstance(field, models.ManyToManyField) and not field.auto_created
-        ]
-
-    def clone_many_to_many_field(new_report, old_report, field_name):
-        old_ids = getattr(old_report, field_name).values_list("id", flat=True)
-        getattr(new_report, field_name).set(old_ids)
-
     def mutate_new_report(new_report, old_report=None):
         if new_report.filter_figure_start_after and new_report.filter_figure_end_before:
             new_report.filter_figure_start_after -= relativedelta(years=YEAR_DIFF)
